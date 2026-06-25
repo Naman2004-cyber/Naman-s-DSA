8aef41fcfdbfb85e7625db7af331763674259007
Naman2004-cyber <namaniit2004@gmail.com>
2026-06-25 10:29:33 +0530

    Time: 82 ms (14.16%), Space: 68.6 MB (9.15%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,16 +1,64 @@
 class Solution {
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
-        int count = 0;
-        for(int i = 0 ; i<nums.size() ; i++)
+    void merge(vector<int> &prefix , int l , int mid , int r , int &count)
+    {
+        int first = l;
+        int second = mid+1;
+        vector<int> temp;
+        while(first <= mid && second <= r )
         {
-            int countTarget = 0;
-            for(int j = i ; j<nums.size() ; j++)
+            if(prefix[first] < prefix[second])
             {
-                if(nums[j] == target) countTarget++;
-                if(countTarget > (j-i+1)/2) count++;
+                count+=(r-second+1);
+                temp.push_back(prefix[first]);
+                first++;
+            }
+            else 
+            {
+                temp.push_back(prefix[second]);
+                second++;
             }
         }
+        while(first <= mid)
+        {
+            temp.push_back(prefix[first]);
+            first++;
+        }
+        while(second <= r)
+        {
+            temp.push_back(prefix[second]);
+            second++;
+        }
+        for(int i = l ; i<=r ; i++)
+        {
+            prefix[i] = temp[i-l];
+        }
+    }
+    void sortu(vector<int> &prefix , int l , int r , int &count)
+    {
+        if(l >= r) return;
+        int mid = (l+r)/2;
+        sortu(prefix , l , mid , count);
+        sortu(prefix , mid+1 , r , count);
+        merge(prefix , l , mid , r , count);
+    }
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        vector<int> transformed(nums.size());
+        for(int i = 0 ; i<nums.size() ; i++)
+        {
+            if(nums[i] == target) transformed[i] = 1;
+            else transformed[i] = -1;
+        }
+        vector<int> prefix;
+        prefix.push_back(0);
+        int sum = 0;
+        for(int i = 0 ; i<nums.size() ; i++)
+        {
+            sum+=transformed[i];
+            prefix.push_back(sum);
+        }
+        int count = 0;
+        sortu(prefix , 0 , prefix.size()-1 , count);
         return count;
     }
 };
