a1f54ae9aea30c8da1c53cb2668955a59b612e7d
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-01 10:39:05 +0530

    Time: 312 ms (87.12%), Space: 136.6 MB (82.88%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -1,74 +1,84 @@
+// in this question we want the minimum distance among all the cells from the thief cells in the path from top left to bottom right to be the maximum possible
+
+// first of all we want to know the minimum distance of all the cells from the thief cells and for the most efficient way is multi source bfs we can start from all the thieves and go in the 4 directions from thieves , so from level 0 to level 1 ,,,, the distance will be 1 now from level 1 cells we will go to all the cells at the level 2 and their distance from the thief cells will eb 2 ,, this is how bfs works ,,, remember once a cell is visited dont visit it again 
+// see always the distance increases by 1 because we are always moving be only 1 edge and we for unvisited cells are always goign away from the thief cells 
+// multisource bfs has t.c of O(n^2) and s.c of O(n^2) and space complexity for storing O(n^2)
+
+// now will we use dijkstra algo in order to find the maximum value of the minimum distance from thieves which has come in the best part from 0,0 to every i,j ,, so the state of distance will store the maximum value of the minimum distance from thieves in the best possible path from 0,0 to i,j ,,,, in the priority queue we will keep on adding the i,j with the minimum value of the cell which has come in the path from 0,0 to i,j ,,,, if we get a path to i,j with has a greater value of the minimum distance from the thieves in the whole path ,,,, then that path will be chosen means the that minimum will be chosen because that will be the maximum possible 
+// time complexity : O((n^2 + n^2)log(n^2)) ----> O(2.n^2log(n)) --> O(n^2log(n))
+// and space complexity is O(n^2)
+
+// so total time compelxity is O(n^2) + O(n^2log(n))  ---> O(n^2logn(n)) and space --> O(n^2)
+
 class Solution {
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        // multi-source bfs to find the minimum distance of each cell from thief cells
         int r[4] = {0,1,0,-1};
         int c[4] = {1,0,-1,0};
+        vector<vector<int>> minchordist(m , vector<int>(n));
         queue<pair<int,int>> qt;
-        vector<vector<int>> minchordist(m , vector<int>(n , 0));
         for(int i = 0 ; i<m ; i++)
         {
             for(int j = 0 ; j<n ; j++)
             {
-                if(grid[i][j] == 1) 
+                if(grid[i][j] == 1)
                 {
-                    qt.push({i , j});
+                    qt.push({i,j});
                     minchordist[i][j] = 0;
                 }
             }
         }
         while(!qt.empty())
         {
-            int a = qt.size();
-            for(int i = 0 ; i<a ; i++)
-            {
-                auto it = qt.front();
-                qt.pop();
-                int currx = it.first;
-                int curry = it.second;
-                for(int j = 0 ; j<4 ; j++)
-                {
-                    int newx = currx + r[j];
-                    int newy = curry + c[j];
-                    if(newx >=0 && newx < m && newy >=0 && newy < n && grid[newx][newy] == 0)
-                    {
-                        minchordist[newx][newy] = minchordist[currx][curry] + 1;
-                        qt.push({newx , newy});
-                        grid[newx][newy] = 1;
-                    }
-                }
-            }
-        }
-        vector<vector<int>> distance(m , vector<int>(n , INT_MIN));
-        priority_queue<pair<int, pair<int, int>>> pq;
-        distance[0][0] = minchordist[0][0];
-        pq.push({minchordist[0][0] , {0 , 0}});
-        while(!pq.empty())
-        {
-            auto it = pq.top();
-            pq.pop();
-            int currminidist = it.first;
-            int currx = it.second.first;
-            int curry = it.second.second;
-            if(currminidist < distance[currx][curry]) continue;
-            if(currx == m-1 && curry == n-1) return currminidist;
+            auto it = qt.front();
+            qt.pop();
+            int currx = it.first;
+            int curry = it.second;
             for(int i = 0 ; i<4 ; i++)
             {
                 int newx = currx + r[i];
                 int newy = curry + c[i];
-                if(newx >=0 && newx < m && newy >=0 && newy < n)
+                if(newx >= 0 && newx < m && newy >= 0 && newy < n && grid[newx][newy] == 0)
                 {
-                    int newminidist = minchordist[newx][newy];
-                    newminidist = min(newminidist , currminidist);
-                    if(newminidist > distance[newx][newy])
-                    {
-                        distance[newx][newy] = newminidist;
-                        pq.push({newminidist , {newx , newy}});
-                    }
+                    grid[newx][newy] = 1;
+                    qt.push({newx , newy});
+                    minchordist[newx][newy] = minchordist[currx][curry] + 1;
+                }
+            }   
+        }
+        //dijkstra algorithm to find the maximum 
+        vector<vector<int>> distance( m , vector<int>(n , INT_MIN));
+        priority_queue<pair<int , pair<int,int>>> pq;
+        pq.push({minchordist[0][0] , {0 , 0}});
+        distance[0][0] = minchordist[0][0];
+        while(!pq.empty())
+        {
+            auto it = pq.top();
+            pq.pop();
+            int currmindist = it.first;
+            int currx = it.second.first;
+            int curry = it.second.second;
+            if(currmindist < distance[currx][curry]) continue;
+            if(currx == m-1 && curry == n-1) return currmindist;
+            for(int i = 0 ; i<4 ; i++)
+            {
+                int newx = currx + r[i];
+                int newy = curry + c[i];
+                if(newx >= 0 && newx < m && newy >= 0 && newy < n)
+                {
+                   int newmindist = minchordist[newx][newy];
+                   newmindist = min(newmindist , currmindist);
+                   if(newmindist > distance[newx][newy])
+                   {
+                        distance[newx][newy] = newmindist;
+                        pq.push({distance[newx][newy] , {newx , newy}});
+                   }
                 }
             }
         }
-        return 0;
+        return -1;
     }
 };
