d216d229c0c29e6f8d2c8c4c135bc14f1db1c2eb
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-02 15:21:52 +0530

    Time: 4 ms (42.17%), Space: 94 MB (38.22%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -15,24 +15,7 @@ public:
             if(nums[r] == maxK) last_seen_max = r;
             if(nums[r] == minK) last_seen_min = r;
             int flag = 0;
-            if(maxi > maxK || mini < minK)
-            {
-                // l = r;
-                // l++;
-                // r++;
-                // if(r < nums.size())
-                // {
-                //     mini = INT_MAX;
-                //     maxi = INT_MIN;
-                //     last_seen_min = -1;
-                //     last_seen_max = -1;
-                //     mini = min(mini , nums[r]);
-                //     maxi = max(maxi , nums[r]);
-                //     if(nums[r] == maxK) last_seen_max = r;
-                //     if(nums[r] == minK) last_seen_min = r;
-                // }
-                flag = 1;
-            }
+            if(maxi > maxK || mini < minK) flag = 1;
             if(flag == 0 && mini == minK && maxi == maxK && (last_seen_min >= l && last_seen_min <=r) && (last_seen_max >= l && last_seen_max <=r))
             {
                 int findmin = min(last_seen_min , last_seen_max);
