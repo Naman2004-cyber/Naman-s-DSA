906371606687d2bfcc7ad67b2a496d50b72bf6e7
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-04 10:43:25 +0530

    Time: 756 ms (20.64%), Space: 133.6 MB (35.15%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        set<vector<int>> st;
+        for(auto &it : points) st.insert(it);
+        int sum = 0;
+        priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> , greater<pair<int , pair<int , int>>>> pq;
+        pq.push({0 , {points[0][0],points[0][1]}});
+        while(!pq.empty())
+        {
+            auto it = pq.top();
+            pq.pop();
+            int currdist = it.first;
+            int currx = it.second.first;
+            int curry = it.second.second;
+            if(st.find({currx , curry}) == st.end()) continue;
+            sum+=currdist;
+            st.erase({currx , curry});
+            for(auto &it : st)
+            {
+                if(it[0] != currx || it[1] != curry)
+                {
+                    int newdist = abs(it[0] - currx) + abs(it[1] - curry);
+                    pq.push({newdist , {it[0] , it[1]}});
+                }
+            }
+        }
+        return sum;
+    }
+};
