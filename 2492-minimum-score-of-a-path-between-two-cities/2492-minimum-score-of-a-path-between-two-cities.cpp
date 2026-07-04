2bd1950538974b758da77d2d507427396a6c7e2b
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-04 08:29:35 +0530

    Time: 56 ms (68.65%), Space: 134.6 MB (49.05%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
+    void doit(int node , vector<vector<pair<int,int>>> &adj , int &mini , vector<int> &visited)
+    {
+        visited[node] = 1;
+        for(auto &it : adj[node])
+        {
+            mini = min(mini , it.second);
+            if(visited[it.first] == 0)
+            {
+                doit(it.first , adj , mini , visited);
+            }
+        }
+    }
     int minScore(int n, vector<vector<int>>& roads) {
+        int mini = INT_MAX;
         vector<vector<pair<int,int>>> adj(n+1);
         for(int i = 0 ; i<roads.size() ; i++)
         {
             adj[roads[i][0]].push_back({roads[i][1] , roads[i][2]});
             adj[roads[i][1]].push_back({roads[i][0] , roads[i][2]});
         }
-        vector<int> mindist(n+1 , 1e9);
-        priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> pq;
-        mindist[1] = 1e9;
-        pq.push({1e9 , 1});
-        while(!pq.empty())
-        {
-            auto it = pq.top();
-            pq.pop();
-            int currmindist = it.first;
-            int currnode = it.second;
-            // if(currmindist > mindist[currnode]) continue;
-            // if(currnode == n) return currmindist;
-            for(auto &a : adj[currnode])
-            {
-                int newnode = a.first;
-                int newmindist = a.second;
-                newmindist = min(newmindist , currmindist);
-                if(newmindist < mindist[newnode])
-                {
-                    mindist[newnode] = newmindist;
-                    pq.push({mindist[newnode] , newnode});
-                }
-            }
-        }
-        return mindist[n];
+        vector<int> visited(n+1 , 0);
+        doit(1 , adj , mini , visited);
+        return mini;
     }
 };
