c3378aba5d4511fc080ba140ce8f662f6af90a58
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-05 12:35:10 +0530

    Time: 146 ms (5.04%), Space: 28.6 MB (83.54%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s, string t) {
-        int i=0,j=0;
-        int n=s.size(),m=t.size();
-        while(i<n&&j<m){
-            if(t[j]!=s[i])++j;
-            else{
-                if(t[j]==s[i])++i,++j;
+        int i = 0;
+        int j = 0;
+        int done = 0;
+        unordered_map<char , int> mpp;
+        unordered_map<char , int> mpp1;
+        for(auto &it : t) mpp[it]++;
+        for(auto &it : s) mpp1[it]++;
+        unordered_map<char , int> lastseent;
+        unordered_map<char , int> lastseens;
+        for(int i = 0 ; i<t.size() ; i++) lastseent[t[i]] = i;
+         for(int i = 0 ; i<s.size() ; i++) lastseens[s[i]] = i;
+        while(i < s.size() && j < t.size())
+        {
+            if(s[i] == t[j])
+            {
+                mpp1[s[i]]--;
+                if(mpp1[s[i]] == 0) mpp1.erase(s[i]);
+                i++;
             }
-        }
-        if(i==n)return true;
-        int lm=i-1;
-        cout<<lm;
-        if(lm==0&&j==m){
-            int lm2=-1;
-            for(char c='a';c<='z';++c){
-                string s1=s;
-                s1[lm2+1]=c;
-                int i=0,j=0;
-                while(i<n&&j<m){
-                    if(t[j]!=s1[i])++j;
-                    else ++i,++j;
-                }
-                if(i==n)return true;
+            else if(done == 0 && (((mpp[s[i]] < mpp1[s[i]]) && (i == lastseens[s[i]] || lastseent[s[i]] == t.size()-1)) || ( (mpp[s[i]] ==  mpp1[s[i]]) && lastseent[s[i]] == t.size()-1)))
+            {
+                done = 1;
+                mpp1[s[i]]--;
+                if(mpp1[s[i]] == 0) mpp1.erase(s[i]);
+                i++;
             }
-            
+            mpp[t[j]]--;
+            if(mpp[t[j]] == 0) mpp.erase(t[j]);
+            j++;
         }
-        for(char c='a';c<='z';++c){
-            string s1=s;
-            s1[lm+1]=c;
-            int i=0,j=0;
-            while(i<n&&j<m){
-                if(t[j]!=s1[i])++j;
-                else ++i,++j;
-            }
-            if(i==n)return true;
-        }
+        if(i == s.size()) return true;
         return false;
     }
 };
