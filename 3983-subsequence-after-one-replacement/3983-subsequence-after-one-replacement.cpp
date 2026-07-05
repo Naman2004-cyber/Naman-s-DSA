11934a8f8f9cb2a69fdb2b9460431de2365c2363
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-05 12:46:08 +0530

    Time: 11 ms (88.99%), Space: 25.9 MB (94.48%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,45 +1,13 @@
 class Solution {
 public:
-    bool canMakeSubsequence(string s, string t) {
-        int i = 0;
-        int j = 0;
-        int done = 0;
-        unordered_map<char , int> mpp;
-        unordered_map<char , int> mpp1;
-        // for(auto &it : t) mpp[it]++;
-        // for(auto &it : s) mpp1[it]++;
-        unordered_map<char , int> lastseent;
-        unordered_map<char , int> lastseens;
-        for(int i = 0 ; i<t.size() ; i++) 
-        {
-            mpp[t[i]]++;
-            lastseent[t[i]] = i;
+    bool canMakeSubsequence(string s, string t) { 
+        int i=0,j=0, m=s.size();
+        for(auto c:t){
+            if(s[i]==c) i++;
+            i=max(i,j+1);
+            if(s[j]==c)  j++;
+            if(i==m || j==m ) return true;
         }
-         for(int i = 0 ; i<s.size() ; i++) 
-         {
-             mpp1[s[i]]++;
-            lastseens[s[i]] = i;
-         }
-        while(i < s.size() && j < t.size())
-        {
-            if(s[i] == t[j])
-            {
-                mpp1[s[i]]--;
-                if(mpp1[s[i]] == 0) mpp1.erase(s[i]);
-                i++;
-            }
-            else if(done == 0 && (((mpp[s[i]] < mpp1[s[i]]) && (i == lastseens[s[i]] || lastseent[s[i]] == t.size()-1)) || ( (mpp[s[i]] ==  mpp1[s[i]]) && lastseent[s[i]] == t.size()-1)))
-            {
-                done = 1;
-                mpp1[s[i]]--;
-                if(mpp1[s[i]] == 0) mpp1.erase(s[i]);
-                i++;
-            }
-            mpp[t[j]]--;
-            if(mpp[t[j]] == 0) mpp.erase(t[j]);
-            j++;
-        }
-        if(i == s.size()) return true;
         return false;
     }
 };
