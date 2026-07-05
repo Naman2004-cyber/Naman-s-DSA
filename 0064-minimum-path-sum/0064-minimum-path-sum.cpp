723ab2e79d88dfdbf9fdb8d4304cde0072b94e62
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-05 15:35:26 +0530

    Time: 0 ms (100%), Space: 15.4 MB (85.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int doit(int currx , int curry , vector<vector<int>>& grid , vector<vector<int>> &dp)
-    {
-        if(currx == grid.size()-1 && curry == grid[0].size()-1) return grid[currx][curry];
-        if(dp[currx][curry] != -1) return dp[currx][curry];
-        int right = INT_MAX;
-        int down = INT_MAX;
-        if(curry+1 < grid[0].size())
-        {
-            right = doit(currx , curry+1 , grid , dp);
-        }
-        if(currx+1 < grid.size())
-        {
-            down = doit(currx+1 , curry , grid , dp);
-        }
-        return dp[currx][curry] =  grid[currx][curry] + min(right , down);
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size() , vector<int>(grid[0].size() , -1));
-        return doit(0 , 0 , grid , dp);
+        int m = grid.size();
+        int n = grid[0].size();
+        // vector<vector<int>> dp(grid.size() , vector<int>(grid[0].size() , -1));
+        vector<int> prev(n , 0);
+        vector<int> curr(n , 0);
+        curr[n-1] = grid[m-1][n-1];
+        // dp[m-1][n-1] = grid[m-1][n-1];
+        for(int i = m-1 ; i>=0 ; i--)
+        {
+            for(int j = n-1 ; j>=0 ; j--)
+            {
+                if(i == m-1 && j == n-1) continue;
+                int right = INT_MAX;
+                int down = INT_MAX;
+                if(j+1 < n) right = grid[i][j] + curr[j+1];
+                if(i+1 < m) down = grid[i][j] + prev[j];
+                curr[j] = min(right , down);
+            }
+            swap(curr , prev);
+        }
+        return prev[0];
     }
 };
