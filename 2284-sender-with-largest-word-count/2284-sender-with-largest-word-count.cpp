8362f85f0b61276771ca7bafd9f3f5f8c94f88d0
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-06 18:03:27 +0530

    Time: 94 ms (18.15%), Space: 97.8 MB (6.25%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        unordered_map<string , string> mpp;
+        for(int i = 0 ; i<messages.size() ; i++)
+        {
+            if(mpp.find(senders[i]) == mpp.end())
+            {
+                mpp[senders[i]] = messages[i];
+            }
+            else
+            {
+                mpp[senders[i]]+=" ";
+                mpp[senders[i]]+=messages[i];
+            }
+        }
+        // for(auto &it : mpp)
+        // {
+        //     cout << it.first << " ---> " << it.second << endl;
+        // }
+        string ans = "";
+        int maxi = 0;
+        for(auto &it : mpp)
+        {
+            string str = it.second;
+            int count = 0;
+            stringstream ss(str);
+            string word;
+            while(ss >> word)
+            {
+                count++;
+            }
+            if(count > maxi)
+            {
+                maxi = count;
+                ans = it.first;
+            }
+            else if(count == maxi && it.first > ans)
+            {
+                ans = it.first;
+            }
+        }
+        return ans;
+    }
+};
