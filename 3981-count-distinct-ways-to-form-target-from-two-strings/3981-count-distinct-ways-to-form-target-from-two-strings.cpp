1e2e73a50400c263b6f14149e79f0a9d68aa9777
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-07 10:43:32 +0530

    Time: 909 ms (64.42%), Space: 13.4 MB (94.27%) - LeetHub

--- 3981-count-distinct-ways-to-form-target-from-two-strings/3981-count-distinct-ways-to-form-target-from-two-strings.cpp
+++ 3981-count-distinct-ways-to-form-target-from-two-strings/3981-count-distinct-ways-to-form-target-from-two-strings.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int mod=1e9+7,dp[101][101][101];
-    string a,b,t;
-    int dfs(int i,int j,int k){
-        if(k==t.size())return i&&j;
-        if(i==a.size()&&j==b.size())return 0;
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
-        long long ans=0;
-        for(int x=i;x<a.size();x++)
-            if(a[x]==t[k])
-                ans=(ans+dfs(x+1,j,k+1))%mod;
-        for(int x=j;x<b.size();x++)
-            if(b[x]==t[k])
-                ans=(ans+dfs(i,x+1,k+1))%mod;
-        return dp[i][j][k]=ans;
+    int dp[101][101][101];
+    int mod = 1e9 + 7;
+    int doit(int word1idx , int word2idx , int targetidx , string &word1 , string &word2 , string &target)
+    {
+        if(targetidx == target.size() && word1idx > 0 && word2idx > 0) return 1;
+        if(word1idx == word1.size() && word2idx == word2.size()) return 0;
+        if(dp[word1idx][word2idx][targetidx] != -1) return dp[word1idx][word2idx][targetidx];
+        long long ans = 0;
+        for(int i = word1idx ; i<word1.size() ; i++)
+        {
+            if(word1[i] == target[targetidx])
+            {
+                ans = (ans + doit(i+1 , word2idx , targetidx+1 , word1 , word2 , target))%mod;
+            }
+        }
+        for(int i = word2idx ; i<word2.size() ; i++)
+        {
+            if(word2[i] == target[targetidx])
+            {
+                ans = (ans + doit(word1idx , i+1 , targetidx+1 , word1 , word2 , target))%mod;
+            }
+        }
+        return dp[word1idx][word2idx][targetidx] = ans;
     }
-    int interleaveCharacters(string word1,string word2,string target){
-        a=word1;
-        b=word2;
-        t=target;
-        memset(dp,-1,sizeof(dp));
-        return dfs(0,0,0);
+    int interleaveCharacters(string word1, string word2, string target) {
+        memset(dp , -1 , sizeof(dp));
+        return doit(0 , 0 , 0 , word1 , word2 , target);
     }
 };
