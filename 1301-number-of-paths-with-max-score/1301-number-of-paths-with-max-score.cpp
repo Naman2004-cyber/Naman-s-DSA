d7e30aede7eea2278b1fcd9b6b9425444d62013d
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-07 08:56:01 +0530

    Time: 6 ms (75.51%), Space: 12.3 MB (33.61%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,73 +1,52 @@
 class Solution {
 public:
-    int mod = 1000000000 + 7;
+    int mod = 1000000000+7;
     int doit(int currx , int curry , vector<vector<int>> &matrix , vector<vector<int>> &dp , vector<vector<int>> &nomaxpath)
     {
         if(currx == 0 && curry == 0) 
         {
-            nomaxpath[0][0] = 1;
+            nomaxpath[currx][curry] = 1;
             return 0;
         }
-        if(dp[currx][curry] != -1) return dp[currx][curry];
-        int left = INT_MIN;
-        int up= INT_MIN;
-        int upleft = INT_MIN;
-        if(curry-1 >=0 && matrix[currx][curry-1] != 0)
-        {
-            left = doit(currx , curry-1 , matrix , dp , nomaxpath);
-        }
-        if(currx-1 >=0 && matrix[currx-1][curry] != 0)
-        {
-            up = doit(currx-1 , curry , matrix , dp , nomaxpath);
-        }
-        if(currx-1 >=0 && curry-1>=0 && matrix[currx-1][curry-1] != 0)
-        {
-            upleft = doit(currx-1 , curry-1 , matrix , dp , nomaxpath);
-        }
-        int maxi = max(left , max(up , upleft));
-        if(maxi == INT_MIN)
-        {
+        if(dp[currx][curry] != INT_MIN) return dp[currx][curry];
+        int left = -1;
+        int up= -1;
+        int upleft = -1;
+        int m = matrix.size() , n = matrix[0].size();
+        if(curry-1 >=0 && matrix[currx][curry-1] != 0) left = doit(currx , curry-1 , matrix , dp , nomaxpath);
+        if(currx-1 >=0 && matrix[currx-1][curry] != 0) up = doit(currx-1 , curry , matrix , dp , nomaxpath);
+        if(currx-1 >=0 && curry-1 >=0 && matrix[currx-1][curry-1] != 0) upleft = doit(currx-1 , curry-1 , matrix , dp , nomaxpath);
+        int maxi = max(max(left , up) , upleft);
+        if(maxi == -1){
             nomaxpath[currx][curry] = 0;
-            return dp[currx][curry] = INT_MIN;
+            return dp[currx][curry] =  -1;
         }
         int count = 0;
-        if(left != INT_MIN && left == maxi && curry-1 >=0 && matrix[currx][curry-1] != 0) count = (count + nomaxpath[currx][curry-1])%mod;
-        if(up != INT_MIN && up == maxi && currx-1 >=0 && matrix[currx-1][curry] != 0) count = (count + nomaxpath[currx-1][curry])%mod;
-        if(upleft != INT_MIN && upleft == maxi && curry-1 >=0 && currx-1 >=0 && matrix[currx-1][curry-1] != 0) count = (count + nomaxpath[currx-1][curry-1])%mod;
-        nomaxpath[currx][curry] = count;
-        if(matrix[currx][curry] == -1) return dp[currx][curry] = max(left , max(up , upleft));
-        return dp[currx][curry] = matrix[currx][curry] + max(left , max(up , upleft));
+        if(left == maxi) count = (count+nomaxpath[currx][curry-1])%mod;
+        if(up == maxi) count = (count+nomaxpath[currx-1][curry])%mod;
+        if(upleft == maxi) count = (count+nomaxpath[currx-1][curry-1])%mod;
+        nomaxpath[currx][curry] = count%mod;
+        if(currx == m-1 && curry == n-1) return dp[currx][curry] = maxi;
+        return dp[currx][curry] = matrix[currx][curry] + maxi;
     }
     vector<int> pathsWithMaxScore(vector<string>& board) {
-        int m = board.size();
-        int n = board[0].size();
+        int m = board.size() , n = board[0].size();
         vector<vector<int>> matrix(m , vector<int>(n));
-        int i = 0;
-        for(auto &it : board)
-        {
-            int j = 0;
-            for(auto &a : it)
-            {
-                if(a == 'E' || a == 'S')
-                {
-                    matrix[i][j] = -1;
-                }
-                else if(a == 'X')
-                {
-                    matrix[i][j] = 0;
-                }
-                else
-                {
-                    matrix[i][j] = a-'0';
-                }
-                j++;
+        int x = 0;
+        for(int i = 0 ; i<board.size() ; i++){
+            int y = 0;
+            for(auto &it : board[i]){
+                if(it == 'E' || it == 'S') matrix[x][y] = -1;
+                else if(it == 'X') matrix[x][y] = 0;
+                else matrix[x][y] = it - '0';
+                y++;
             }
-            i++;
+            x++;
         }
-        vector<vector<int>> dp(m , vector<int>(n , -1));
+        vector<vector<int>> dp(m , vector<int>(n , INT_MIN));
         vector<vector<int>> nomaxpath(m , vector<int>(n , 0));
         int ans = doit(m-1 , n-1 , matrix , dp , nomaxpath);
-        if(ans == INT_MIN) return {0,0};
+        if(ans == -1) return {0,0};
         return {ans , nomaxpath[m-1][n-1]};
     }
 };
