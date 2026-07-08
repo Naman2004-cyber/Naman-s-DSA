fb8ecaaa8f6b608c7db4a281e1b537a15905da2c
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-08 16:21:08 +0530

    Time: 48 ms (44.04%), Space: 157 MB (48.19%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,66 +1,44 @@
 class Solution {
 public:
-    long long mod = 1e9+7;
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        long long mod = 1e9+7;
         int n = s.size();
-        int sum = 0;
-        vector<int> pre(n);
-        for(int i = 0 ; i<s.size() ; i++)
-        {
-            int a = s[i]-'0';
-            pre[i] = sum;
-            sum+=a;
-        }
-        vector<int> suff(n);
-        int sum1 = 0;
-        for(int i = s.size()-1 ; i>=0 ; i--)
-        {
-            int a = s[i]-'0';
-            suff[i] = sum1;
-            sum1+=a;
-        }
-        vector<int> numr(n);
-        vector<int> zeros(n);
-        int count0 = 0;
-        int number = 0;
-        for(int i = 0; i<s.size() ; i++)
-        {
-            int a = s[i]-'0';
-            if(a != 0)
-            {
-                number = (1LL*number*10)%mod + a;
-                count0++;
+        vector<long long> sumTillIdx(n);
+        vector<long long> numTillIdx(n);
+        vector<int> nonZeroNums(n);
+        long long sum = 0;
+        long long num = 0;
+        int countNonZero = 0;
+        for(int i = 0 ; i<n ; i++){
+            if(s[i] != '0'){
+                int a = s[i]-'0';
+                sum = (sum+a)%mod;
+                num = (1LL*num*10)%mod + a;
+                countNonZero++;
             }
-            numr[i] = number;
-            zeros[i] = count0;
+            sumTillIdx[i] = sum;
+            numTillIdx[i] = num;
+            nonZeroNums[i] = countNonZero;
         }
-        vector<int> ans;
-
         vector<long long> pow_of_10(n);
         long long val = 1;
         for(int i = 0 ; i<n ; i++){
             pow_of_10[i] = val;
-            val = (1LL * val * 10)%mod;
+            val = (1LL*val*10)%mod;
         }
-
-        for(int i = 0 ; i<queries.size() ; i++)
-        {
-            int l = queries[i][0];
-            int r = queries[i][1];
-            long long numtillr = numr[r];
-            int numbeforel = 0;
-            int zeroneeded = 0;
-            if(l-1 >= 0)
-            {
-                numbeforel = numr[l-1];
-                zeroneeded = zeros[r] - zeros[l-1];
-            }
-            long long powmul = pow_of_10[zeroneeded];
-            numbeforel = (1LL * numbeforel*powmul)%mod;
-            int finalnumber = (numtillr - numbeforel + mod)%mod;
-            int reqsum = sum - pre[l] - suff[r];
-            int finalans = (1LL * finalnumber*reqsum)%mod;
-            ans.push_back(finalans);
+        vector<int> ans;
+        for(int i = 0 ; i<queries.size() ; i++){
+            int l = queries[i][0] , r = queries[i][1];
+            long long reqSum = sumTillIdx[r];
+            if(l > 0) reqSum = reqSum - sumTillIdx[l-1];
+            long long numr = numTillIdx[r];
+            long long numl = 0;
+            long long reqZeros = nonZeroNums[r];
+            if(l > 0) reqZeros = reqZeros - nonZeroNums[l-1];
+            if(l > 0) numl = (1LL * numTillIdx[l-1]*pow_of_10[reqZeros])%mod;
+            long long reqProd = (numr - numl + mod)%mod;
+            long long finalnum = (1LL*reqSum*reqProd)%mod;
+            ans.push_back(finalnum);
         }
         return ans;
     }
