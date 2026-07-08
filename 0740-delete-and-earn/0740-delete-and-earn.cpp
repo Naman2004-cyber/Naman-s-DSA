fc034f0b99a249a4b6d54043b255172961b0e709
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-08 18:59:22 +0530

    Time: 15 ms (6.68%), Space: 41 MB (5.29%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int doit(int index , vector<int> &use , unordered_map<int , int> &mpp , vector<int> &dp)
+    {
+        if(index >= use.size()) return 0;
+        if(dp[index] != -1) return dp[index];
+        int skip = doit(index+1 , use , mpp , dp);
+        int take = 0;
+        if(index+1 < use.size() && use[index+1] == use[index]+1){
+            take = use[index]*mpp[use[index]] + doit(index+2 , use , mpp , dp);
+        }
+        else{
+            take = use[index]*mpp[use[index]] + doit(index+1 , use , mpp , dp);
+        }
+        return dp[index] = max(take , skip);
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        vector<int> use;
+        unordered_map<int , int> mpp;
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(mpp.find(nums[i]) == mpp.end()) use.push_back(nums[i]);
+            mpp[nums[i]]++;
+        }
+        sort(use.begin() , use.end());
+        vector<int> dp(100000 , -1);
+        return doit(0 , use , mpp , dp);
+    }
+};
