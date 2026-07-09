cf781ded8553a70407db08f9eeb20c03354b847f
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-09 10:40:03 +0530

    Time: 2223 ms (5.03%), Space: 523.2 MB (5.03%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int doit(int index , int prevSwap , vector<int>& nums1, vector<int>& nums2 , vector<vector<int>> &dp)
+    {
+        if(index == nums1.size()) return 0;
+        if(dp[index][prevSwap] != -1) return dp[index][prevSwap];
+        int prev1 = nums1[index-1];
+        int prev2 = nums2[index-1];
+        if(prevSwap) swap(prev1 , prev2);
+        int ans = 1e9;
+        // dont swap
+        if(prev1 < nums1[index] && prev2 < nums2[index]){
+            ans = min(ans , doit(index+1 , 0 , nums1 , nums2 , dp));
+        }
+        // swap if condition
+        if(prev1 < nums2[index] && prev2 < nums1[index]){
+            ans = min(ans , 1 + doit(index+1 , 1 , nums1 , nums2 , dp));
+        }
+
+        return dp[index][prevSwap] = ans;
+
+    }
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 1e9;
+        vector<vector<int>> dp(100000+1 , vector<int>(2 , -1));
+        // dont swap 0
+        ans = min(ans , doit(1 , 0 , nums1 , nums2 , dp));
+        // swap 0
+        ans = min(ans , 1 + doit(1 , 1 , nums1 , nums2 , dp));
+        return ans;
+    }
+};
