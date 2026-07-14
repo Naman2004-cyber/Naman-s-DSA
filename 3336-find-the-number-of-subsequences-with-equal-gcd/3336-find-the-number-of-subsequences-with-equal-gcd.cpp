d7dbc980490078aaf95e4b8b2bc1060d37314a5c
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-14 13:10:44 +0530

    Time: 1147 ms (46.09%), Space: 58.3 MB (62.61%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mod = 1e9+7;
     int dp[201][201][201];
-    int doit(int index , vector<int>& nums , int seq1 , int seq2 , int dp[][201][201]){
+    int doit(int index , vector<int>& nums , int seq1 , int seq2){
         if(index == nums.size()){
             if(seq1 == seq2 && seq1 != 0) return 1;
             return 0;
@@ -13,15 +13,15 @@ public:
         int take1 = 0;
         int take2 = 0;
         int skip = 0;
-        take1 = take1 + doit(index+1 , nums , use1 , seq2 , dp);
-        take2 = take2 + doit(index+1 , nums , seq1 , use2 , dp);
-        skip = skip + doit(index+1 , nums , seq1 , seq2 , dp);
+        take1 = take1 + doit(index+1 , nums , use1 , seq2);
+        take2 = take2 + doit(index+1 , nums , seq1 , use2);
+        skip = skip + doit(index+1 , nums , seq1 , seq2);
         return dp[index][seq1][seq2] = ((take1+take2)%mod+skip)%mod;
     }
     int subsequencePairCount(vector<int>& nums) {
         int seq1 = 0;
         int seq2 = 0;
         memset(dp , -1 , sizeof(dp));
-        return doit(0 , nums , seq1 , seq2 , dp);
+        return doit(0 , nums , seq1 , seq2);
     }
 };
