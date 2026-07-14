6cef58f17a16c7aa5b2422ef83f5d4cb023c0575
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-14 12:49:44 +0530

    Time: 1082 ms (49.56%), Space: 58.2 MB (69.56%) - LeetHub

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int dp[201][201][201];
+    int doit(int index , vector<int>& nums , int seq1 , int seq2 , int dp[][201][201]){
+        if(index == nums.size()){
+            if(seq1 == seq2 && seq1 != 0) return 1;
+            return 0;
+        }
+        if(dp[index][seq1][seq2] != -1) return dp[index][seq1][seq2];
+        int use1 = gcd(nums[index] , seq1);
+        int use2 = gcd(nums[index] , seq2);
+        int take1 = 0;
+        int take2 = 0;
+        int skip = 0;
+        take1 = take1 + doit(index+1 , nums , use1 , seq2 , dp);
+        take2 = take2 + doit(index+1 , nums , seq1 , use2 , dp);
+        skip = skip + doit(index+1 , nums , seq1 , seq2 , dp);
+        return dp[index][seq1][seq2] = ((take1+take2)%mod+skip)%mod;
+    }
+    int subsequencePairCount(vector<int>& nums) {
+        int seq1 = 0;
+        int seq2 = 0;
+        memset(dp , -1 , sizeof(dp));
+        return doit(0 , nums , seq1 , seq2 , dp);
+    }
+};
