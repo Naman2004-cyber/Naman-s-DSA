c1cd476aa3e62e7562adb28b6dc394f78117db45
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-14 13:29:25 +0530

    Time: 2478 ms (5.21%), Space: 58.3 MB (62.61%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int mod = 1e9+7;
     int dp[201][201][201];
+    int gcdTable[201][201];
     int doit(int index , vector<int>& nums , int seq1 , int seq2){
         if(index == nums.size()){
             if(seq1 == seq2 && seq1 != 0) return 1;
             return 0;
         }
         if(dp[index][seq1][seq2] != -1) return dp[index][seq1][seq2];
-        int use1 = gcd(nums[index] , seq1);
-        int use2 = gcd(nums[index] , seq2);
+        int use1 = gcdTable[nums[index]][seq1];
+        int use2 = gcdTable[nums[index]][seq2];
         int take1 = 0;
         int take2 = 0;
         int skip = 0;
@@ -19,6 +20,11 @@ public:
         return dp[index][seq1][seq2] = ((take1+take2)%mod+skip)%mod;
     }
     int subsequencePairCount(vector<int>& nums) {
+        for(int i = 0 ; i<=200 ; i++){
+            for(int j = 0 ; j<=200 ; j++){
+                gcdTable[i][j] = gcd(i , j);
+            }
+        }
         int seq1 = 0;
         int seq2 = 0;
         memset(dp , -1 , sizeof(dp));
