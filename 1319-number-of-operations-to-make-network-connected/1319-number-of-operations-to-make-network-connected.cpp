ad0d92f1ccc021d0584a0f7b616aa500fc899a71
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-14 08:34:25 +0530

    Time: 11 ms (64.78%), Space: 43.7 MB (66.32%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int find(int node , vector<int> &parent){
+        if(parent[node] == node) return node;
+        return parent[node] = find(parent[node] , parent);
+    }
+    void doUnion(int parentu , int parentv , vector<int> &parent , vector<int> &rank){
+        if(rank[parentu] > rank[parentv]){
+            parent[parentv] = parentu;
+        }
+        else if(rank[parentv] > rank[parentu]){
+            parent[parentu] = parentv;
+        }
+        else{
+            parent[parentv] = parentu;
+            rank[parentu]++;
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n-1) return -1;
+        vector<int> parent(n);
+        vector<int> rank(n , 0);
+        for(int i = 0 ; i<n ; i++) parent[i] = i;
+        for(int i = 0 ; i<connections.size() ; i++){
+            int u = connections[i][0] , v = connections[i][1];
+            int parentu = find(u , parent);
+            int parentv = find(v , parent);
+            if(parentu != parentv){
+                doUnion(parentu , parentv , parent , rank);
+            }
+        }
+        unordered_set<int> st;
+        for(int i = 0 ; i<parent.size() ; i++) {
+            find(i , parent);
+            st.insert(parent[i]);
+        }
+        return st.size()-1;
+    }
+};
