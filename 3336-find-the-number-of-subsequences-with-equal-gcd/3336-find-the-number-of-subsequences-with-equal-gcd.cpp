c30e58be437f92bbf75fcfc81e37ab591c93f208
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-15 11:20:22 +0530

    Time: 2295 ms (5.21%), Space: 58.4 MB (62.61%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int dp[201][201][201];
+    int gcdTable[201][201];
     int mod = 1e9+7;
     int doit(int index , vector<int>& nums , int seq1 , int seq2){
         if(index == nums.size()){
@@ -9,8 +10,8 @@ public:
         }
         if(dp[index][seq1][seq2] != -1) return dp[index][seq1][seq2];
         int take1 = 0 , take2 = 0 , skip = 0;
-        int newSeq1 = gcd(seq1 , nums[index]);
-        int newSeq2 = gcd(seq2 , nums[index]);
+        int newSeq1 = gcdTable[seq1][nums[index]];
+        int newSeq2 = gcdTable[seq2][nums[index]];
         take1 = take1 + doit(index+1 , nums , newSeq1 , seq2);
         take2 = take2 + doit(index+1 , nums , seq1 , newSeq2);
         skip = skip + doit(index+1 , nums , seq1 , seq2);
@@ -19,6 +20,12 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         int seq1 = 0;
         int seq2 = 0;
+        for(int i = 0 ; i<=200 ; i++){
+            for(int j = i ; j<=200 ; j++){
+                gcdTable[i][j] = gcd(i , j);
+                gcdTable[j][i] = gcdTable[i][j];
+            }
+        }
         memset(dp , -1 , sizeof(dp));
         return doit(0 , nums , seq1 , seq2);
     }
