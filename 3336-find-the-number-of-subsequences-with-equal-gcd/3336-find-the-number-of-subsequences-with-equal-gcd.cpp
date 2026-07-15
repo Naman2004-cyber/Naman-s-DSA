59702aec827bdf854c29ed80e88531463be6935f
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-15 11:41:43 +0530

    Time: 2724 ms (5.21%), Space: 58.5 MB (60.87%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -3,30 +3,37 @@ public:
     int dp[201][201][201];
     int gcdTable[201][201];
     int mod = 1e9+7;
-    int doit(int index , vector<int>& nums , int seq1 , int seq2){
-        if(index == nums.size()){
-            if(seq1 == seq2 && seq1 != 0) return 1;
-            return 0;
-        }
-        if(dp[index][seq1][seq2] != -1) return dp[index][seq1][seq2];
-        int take1 = 0 , take2 = 0 , skip = 0;
-        int newSeq1 = gcdTable[seq1][nums[index]];
-        int newSeq2 = gcdTable[seq2][nums[index]];
-        take1 = take1 + doit(index+1 , nums , newSeq1 , seq2);
-        take2 = take2 + doit(index+1 , nums , seq1 , newSeq2);
-        skip = skip + doit(index+1 , nums , seq1 , seq2);
-        return dp[index][seq1][seq2] = ((take1 + take2)%mod + skip)%mod;
-    }
     int subsequencePairCount(vector<int>& nums) {
-        int seq1 = 0;
-        int seq2 = 0;
         for(int i = 0 ; i<=200 ; i++){
             for(int j = i ; j<=200 ; j++){
                 gcdTable[i][j] = gcd(i , j);
                 gcdTable[j][i] = gcdTable[i][j];
             }
         }
-        memset(dp , -1 , sizeof(dp));
-        return doit(0 , nums , seq1 , seq2);
+        int n = nums.size();
+        for(int i = 0 ; i<=200 ; i++){
+            for(int j = 0 ; j<=200 ; j++){
+                if(i == j && i != 0){
+                    dp[n][i][j] = 1;
+                }
+                else{
+                    dp[n][i][j] = 0;
+                }
+            }
+        }
+        for(int index = n-1 ; index>=0 ; index--){
+            for(int i = 200 ; i>=0 ; i--){
+                for(int j = 200 ; j>=0 ; j--){
+                    int newSeq1 = gcdTable[i][nums[index]];
+                    int newSeq2 = gcdTable[j][nums[index]];
+                    int ans = 0;
+                    ans = (ans + dp[index+1][newSeq1][j])%mod;
+                    ans = (ans + dp[index+1][i][newSeq2])%mod;
+                    ans = (ans + dp[index+1][i][j])%mod;
+                    dp[index][i][j] = ans;
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 };
