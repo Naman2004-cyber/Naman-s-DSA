93619e193908cd02ab6aa022253e45fadb488db7
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-15 15:19:10 +0530

    Time: 9 ms (72.47%), Space: 9 MB (82.26%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int dp[501][501];
     int mod = 1e9+7;
-    int doit(int index , int currSteps , int arrLen , int steps){
-        if(currSteps == steps){
-            if(index == 0) return 1;
-            return 0;
-        }
-        if(index > steps-currSteps) return 0;
-        if(dp[index][currSteps] != -1) return dp[index][currSteps];
-        int left = 0 , right = 0 , stay = 0;
-        if(index-1 >= 0) left = left + doit(index-1 , currSteps+1 , arrLen , steps);
-        if(index+1 < arrLen) right = right + doit(index+1 , currSteps+1 , arrLen , steps);
-        stay = stay + doit(index , currSteps+1 , arrLen , steps);
-        return dp[index][currSteps] = ((left+right)%mod+stay)%mod;
-    }
     int numWays(int steps, int arrLen) {
-        memset(dp , -1 , sizeof(dp));
-        return doit(0 , 0 , arrLen , steps);
+        arrLen = min(arrLen , steps+1);
+        for(int i = 0 ; i<arrLen ; i++){
+            if(i == 0) dp[i][steps] = 1;
+            else dp[i][steps] = 0;
+        }
+        for(int currSteps = steps-1 ; currSteps>=0 ; currSteps--){
+            for(int index = steps ; index>=0 ; index--){
+                int ans = 0;
+                if(index-1 >= 0) ans = (ans + dp[index-1][currSteps+1])%mod;
+                if(index+1 < arrLen) ans = (ans + dp[index+1][currSteps+1])%mod;
+                ans = (ans + dp[index][currSteps+1])%mod;
+                dp[index][currSteps] = ans;
+            }
+        }
+        return dp[0][0];
     }
 };
