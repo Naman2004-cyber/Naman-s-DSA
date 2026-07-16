a4b7f8ed8030d88f11e131d02975466cdb19b870
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-16 14:54:34 +0530

    Time: 34 ms (40.84%), Space: 29.3 MB (8.76%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -4,16 +4,23 @@ public:
     unordered_map<int , vector<int>> columns;
     void doit(int index , vector<vector<int>>& stones , vector<int> &visited){
         visited[index] = 1;
-        for(auto &it : rows[stones[index][0]]){
+        int r = stones[index][0];
+        int c = stones[index][1];
+        auto rowList = rows[r];
+        rows.erase(r);
+        for(auto &it : rowList){
             if(visited[it] == 0){
                 doit(it , stones , visited);
             }
         }
-        for(auto &it : columns[stones[index][1]]){
+        auto colList = columns[c];
+        columns.erase(c);
+        for(auto &it : colList){
             if(visited[it] == 0){
                 doit(it , stones , visited);
             }
         }
+        columns.erase(stones[index][1]);
     }
     int removeStones(vector<vector<int>>& stones) {
         for(int i = 0 ; i<stones.size() ; i++){
