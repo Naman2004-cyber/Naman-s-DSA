7962bd484195723bfa46273e1dc0e9153482080e
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-20 14:04:12 +0530

    Time: 23 ms (69.84%), Space: 12.5 MB (83.14%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -3,13 +3,21 @@ public:
     int numTeams(vector<int>& rating) {
         int n = rating.size();
         int count = 0;
-        for(int i = 0 ; i<n-2 ; i++){
-            for(int j = i+1 ; j<n-1 ; j++){
-                for(int k = j+1 ; k<n ; k++){
-                    if(rating[i] > rating[j] && rating[j] > rating[k]) count++;
-                    if(rating[i] < rating[j] && rating[j] < rating[k]) count++;
-                }
+        for(int i = 0 ; i<n ; i++){
+            int countSmallRight = 0;
+            int countLargeRight = 0;
+            for(int j = i+1 ; j<n ; j++){
+                if(rating[j] > rating[i]) countLargeRight++;
+                if(rating[j] < rating[i]) countSmallRight++;
             }
+            int countSmallLeft = 0;
+            int countLargeLeft = 0;
+            for(int j = 0 ; j<i ; j++){
+                if(rating[j] > rating[i]) countLargeLeft++;
+                if(rating[j] < rating[i]) countSmallLeft++;
+            }
+            count+=(countSmallLeft * countLargeRight);
+            count+=(countLargeLeft * countSmallRight);
         }
         return count;
     }
