aa3feb50f089cbb8cae60fedfdb02df35221ecd9
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-20 18:27:21 +0530

    Time: 73 ms (13.33%), Space: 112.6 MB (13.84%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<long long>> distance{26 , vector<long long>(26 , LLONG_MAX)};
+    void doit(int source , vector<vector<pair<int,int>>> &adj){
+        priority_queue<pair<long long , int> , vector<pair<long long,int>> , greater<pair<long long ,int>>> pq;
+        distance[source][source] = 0;
+        pq.push({0 , source});
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            long long currcost = it.first;
+            int currnode = it.second;
+            if(currcost > distance[source][currnode]) continue;
+            for(auto &it : adj[currnode]){
+                int newnode = it.first;
+                long long newcost = it.second;
+                if(newcost + currcost < distance[source][newnode]){
+                    distance[source][newnode] = newcost + currcost;
+                    pq.push({currcost + newcost , newnode});
+                }
+            }
+        }
+    }
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<pair<int,int>>> adj(26);
+        for(int i = 0 ; i<original.size() ; i++){
+            adj[original[i]-'a'].push_back({changed[i]-'a' , cost[i]});
+        }
+        //dijkstra on each character which is not equal
+        for(int i = 0 ; i<26 ; i++){
+            doit(i , adj);
+        }
+        long long finalans = 0;
+        for(int i = 0 ; i<source.size() ; i++){
+            if(source[i] != target[i]){
+                if(distance[source[i]-'a'][target[i]-'a'] == LLONG_MAX) return -1;
+                finalans = finalans + distance[source[i]-'a'][target[i]-'a'];
+            }
+        }
+        return finalans;
+    }
+};
