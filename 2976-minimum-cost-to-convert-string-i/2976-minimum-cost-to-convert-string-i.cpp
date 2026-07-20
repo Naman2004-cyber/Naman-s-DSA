43c46869205b939ca700ed4f006977f65e056793
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-20 19:54:24 +0530

    Time: 50 ms (31.74%), Space: 116.5 MB (11.5%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<vector<long long>> distance{26 , vector<long long>(26 , LLONG_MAX)};
-    void doit(int source , vector<vector<pair<int,long long>>> &adj){
-        priority_queue<pair<long long , int> , vector<pair<long long,int>> , greater<pair<long long ,int>>> pq;
+    void doDijkstra(int source , vector<vector<pair<int , long long>>> &adj){
+        priority_queue<pair<long long , int> , vector<pair<long long , int>> , greater<pair<long long , int>>> pq;
+        pq.push({0, source});
         distance[source][source] = 0;
-        pq.push({0 , source});
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
             long long currcost = it.first;
             int currnode = it.second;
             if(currcost > distance[source][currnode]) continue;
-            for(auto &it : adj[currnode]){
-                int newnode = it.first;
-                long long newcost = it.second;
-                if(newcost + currcost < distance[source][newnode]){
-                    distance[source][newnode] = newcost + currcost;
-                    pq.push({currcost + newcost , newnode});
+            for(auto &a : adj[currnode]){
+                int newnode = a.first;
+                long long newcost = a.second;
+                if(currcost + newcost < distance[source][newnode]){
+                    distance[source][newnode] = currcost + newcost;
+                    pq.push({distance[source][newnode] , newnode});
                 }
             }
         }
     }
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector<vector<pair<int,long long >>> adj(26);
+        vector<vector<pair<int , long long>>> adj(26);
         for(int i = 0 ; i<original.size() ; i++){
             adj[original[i]-'a'].push_back({changed[i]-'a' , cost[i]});
         }
-        //dijkstra on each character which is not equal
         for(int i = 0 ; i<26 ; i++){
-            doit(i , adj);
+            doDijkstra(i , adj);
         }
         long long finalans = 0;
         for(int i = 0 ; i<source.size() ; i++){
             if(source[i] != target[i]){
-                if(distance[source[i]-'a'][target[i]-'a'] == LLONG_MAX) return -1;
-                finalans = finalans + distance[source[i]-'a'][target[i]-'a'];
+                long long ans = distance[source[i]-'a'][target[i]-'a'];
+                if(ans == LLONG_MAX) return -1;
+                finalans+= distance[source[i]-'a'][target[i]-'a'];
             }
         }
         return finalans;
