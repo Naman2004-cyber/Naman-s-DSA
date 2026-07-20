70bcacda497fcb48878336457025e6f6acfde71b
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-20 14:40:07 +0530

    Time: 11 ms (54.21%), Space: 34.7 MB (59.97%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -11,31 +11,35 @@
  */
 class FindElements {
 public:
+    unordered_set<int> st;
     void doit(TreeNode* node){
         if(node == NULL) return;
         if(node->left != NULL){
             node->left->val = 2*node->val + 1;
+            st.insert(node->left->val);
         }
         if(node->right != NULL){
             node->right->val = 2*node->val + 2;
+            st.insert(node->right->val);
         }
         doit(node->left);
         doit(node->right);
     }
-    bool pataKaro(TreeNode* node , int target){
-        if(node == NULL) return false;
-        if(node->val == target) return true;
-        return pataKaro(node->left , target) || pataKaro(node->right , target);
-    }
+    // bool pataKaro(TreeNode* node , int target){
+    //     if(node == NULL) return false;
+    //     if(node->val == target) return true;
+    //     return pataKaro(node->left , target) || pataKaro(node->right , target);
+    // }
     TreeNode* use;
     FindElements(TreeNode* root) {
         use = root;
         root->val = 0;
+        st.insert(root->val);
         doit(root);
     }
     
     bool find(int target) {
-        return pataKaro(use , target);
+        return st.find(target) != st.end();
     }
 };
 
