0f966858909ea0e84b548cb36c1ce7a691b7ddd9
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-20 14:40:36 +0530

    Time: 9 ms (63.08%), Space: 34.7 MB (42.68%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -25,19 +25,11 @@ public:
         doit(node->left);
         doit(node->right);
     }
-    // bool pataKaro(TreeNode* node , int target){
-    //     if(node == NULL) return false;
-    //     if(node->val == target) return true;
-    //     return pataKaro(node->left , target) || pataKaro(node->right , target);
-    // }
-    TreeNode* use;
     FindElements(TreeNode* root) {
-        use = root;
         root->val = 0;
         st.insert(root->val);
         doit(root);
     }
-    
     bool find(int target) {
         return st.find(target) != st.end();
     }
