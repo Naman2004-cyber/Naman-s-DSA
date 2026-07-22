ac7bffcec29926caea1e4ecf0861be0058302c1a
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-23 00:50:53 +0530

    Time: 7 ms (68.76%), Space: 8.2 MB (44.79%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -8,12 +8,12 @@ public:
         int ans = 0;
         for(int digit = 0 ; digit<=limit ; digit++){
             int nextStarted = started || (digit != 0);
-            int nextTight = tight && (digit == str[idx]-'0');
+            int nextTight = tight && (digit == limit);
             if(!nextStarted){
                 ans+=doit(idx+1 , mask , nextTight , false , str);
             }
             else{
-                 if(mask & (1 << digit)) continue;
+                if(mask & (1 << digit)) continue;
                 int newMask = (mask | (1 << digit));
                 ans+=doit(idx+1 , newMask , nextTight , nextStarted , str);
             }
