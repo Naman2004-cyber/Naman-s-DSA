753756d67bde924ad666b8f8bc29a306cb8ca50e
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-24 01:16:53 +0530

    Time: 0 ms (100%), Space: 198.7 MB (98.14%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -3,7 +3,6 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int number = nums.size();
         if(number <= 2) return number;
-        vector<int> store;
         int index = 0;
         int val = 1;
         for(int i = 0 ; i<=16 ; i++){
@@ -12,7 +11,7 @@ public:
             index++;
         }
         cout << index;
-        int ans = pow(2 , index+1);
+        int ans = 1 << (index+1);
         return ans;
     }
 };
