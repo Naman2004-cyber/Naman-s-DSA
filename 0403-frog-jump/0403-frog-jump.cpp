4d3ab2dcd74b625dda2c6af1a234bda86f0400d3
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-28 13:29:54 +0530

    Time: 113 ms (37.11%), Space: 54.4 MB (48.1%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    unordered_map<long long , bool> positions;
     unordered_map<long long , bool> dp;
-    bool doit(long long idx , long long prev , set<int>& st , int end){
-        if(idx == end) return true;
-        long long key = (idx << 32) | prev;
+    bool doit(long long idx , long long prevJumps , int last){
+        if(idx == last) return true;
+        long long key = (idx << 32) | prevJumps;
         if(dp.count(key)) return dp[key];
-        bool first = false;
-        bool second = false;
-        bool third = false;
-        if(prev-1 > 0 && st.count(idx + prev-1)){
-            first = doit(idx+prev-1 , prev-1 , st , end);
+        bool prev_minus = false;
+        bool prev_equal = false;
+        bool prev_plus = false;
+        if((prevJumps-1 > 0) && positions.find(idx + prevJumps -1) != positions.end()){
+            prev_minus = doit(idx+prevJumps-1 , prevJumps-1 , last);
         }
-        if(st.count(idx+prev)){
-            second = doit(idx+prev , prev , st , end);
+        if((prevJumps > 0) && positions.find(idx + prevJumps) != positions.end()){
+            prev_equal = doit(idx+prevJumps , prevJumps , last);
         }
-        if(st.count(idx+prev+1)){
-            third = doit(idx+prev+1 , prev+1 , st , end);
+        if((prevJumps+1 > 0) && positions.find(idx + prevJumps + 1) != positions.end()){
+            prev_plus = doit(idx+prevJumps+1 , prevJumps+1 , last);
         }
-        return dp[key] = first || second || third;
+        return dp[key] = prev_minus || prev_equal || prev_plus;
     }
     bool canCross(vector<int>& stones) {
-        set<int> st;
-        st.insert(stones.begin() , stones.end());
-        if(!st.count(1)) return false;
-        return doit(1 , 1 , st , stones[stones.size()-1]);
+        if(stones[1] != 1) return false;
+        for(int i = 0 ; i<stones.size() ; i++) positions[stones[i]] = true;
+        return doit(1 , 1 , stones[stones.size()-1]);
     }
 };
