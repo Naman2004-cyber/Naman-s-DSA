4c26125177e139053a80922a22440274300e9208
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-28 15:19:26 +0530

    Time: 63 ms (82.33%), Space: 21.1 MB (98.89%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int dp[2001][2001];
-    unordered_map<int , int> posToIdx;
-    bool doit(int idx , int prevJump , vector<int>& stones){
-        if(idx == stones.size()-1) return true;
-        if(dp[idx][prevJump] != -1) return dp[idx][prevJump];
-        bool prev_minus = false;
-        bool prev_equal = false;
-        bool prev_plus = false;
-        if(prevJump-1 > 0){
-            int newPos = stones[idx] + prevJump - 1;
-            if(posToIdx.find(newPos) != posToIdx.end()){
-                prev_minus = doit(posToIdx[newPos] , prevJump - 1 , stones);
-            }
-        }
-        if(prevJump > 0){
-            int newPos = stones[idx] + prevJump;
-            if(posToIdx.find(newPos) != posToIdx.end()){
-                prev_equal = doit(posToIdx[newPos] , prevJump , stones);
-            }
-        }
-        if(prevJump+1 > 0){
-            int newPos = stones[idx] + prevJump + 1;
-            if(posToIdx.find(newPos) != posToIdx.end()){
-                prev_plus = doit(posToIdx[newPos] , prevJump +1 , stones);
-            }
-        }
-        return dp[idx][prevJump] = prev_minus || prev_equal || prev_plus;
-    }
+    bool dp[2001][2001];
     bool canCross(vector<int>& stones) {
         if(stones[1] != 1) return false;
-        for(int i = 0 ; i<stones.size() ; i++) posToIdx[stones[i]] = i;
-        memset(dp , -1 , sizeof(dp));
-        return doit(1 , 1 , stones);
+        int n = stones.size();
+        unordered_map<int , int> posToIdx;
+        for(int i = 0 ; i<n ; i++) posToIdx[stones[i]] = i;
+        memset(dp , false , sizeof(dp));
+        dp[1][1] = 1;
+        for(int idx = 1 ; idx<n ; idx++){
+            for(int prevJumps = 1 ; prevJumps <= n ; prevJumps++){
+                if(!dp[idx][prevJumps]) continue;
+                if(prevJumps-1 > 0){
+                    int newPos1 = stones[idx] + prevJumps - 1;
+                    if(posToIdx.find(newPos1) != posToIdx.end()) dp[posToIdx[newPos1]][prevJumps-1] = true;
+                }
+
+                int newPos2 = stones[idx] + prevJumps;
+                if(posToIdx.find(newPos2) != posToIdx.end()) dp[posToIdx[newPos2]][prevJumps] = true;
+
+                int newPos3 = stones[idx] + prevJumps + 1;
+                if(posToIdx.find(newPos3) != posToIdx.end()) dp[posToIdx[newPos3]][prevJumps+1] = true;
+            }
+        }
+        for(int jumps = 1 ; jumps <= n ; jumps++){
+            if(dp[n-1][jumps]) return true;
+        }
+        return false;
     }
 };
