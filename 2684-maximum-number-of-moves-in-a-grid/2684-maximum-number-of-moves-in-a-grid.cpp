800b4309ad1ea0e6dab7be1248ffb24064fc6bf9
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-29 19:20:35 +0530

    Time: 72 ms (8.24%), Space: 71.7 MB (87.73%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int dp[1000][1000];
-    int doit(int currx , int curry , int endx , int endy , vector<vector<int>>& grid , int prev){
-        if(currx <0 || currx >= endx || curry < 0 || curry >= endy) return 0;
-        if(grid[currx][curry] <= prev) return 0;
-        if(dp[currx][curry] != -1) return dp[currx][curry];
+    int doit(int currx , int curry , vector<vector<int>>& grid){
+        int right = 0;
         int rightDown = 0;
         int rightUp = 0;
-        int right = 0;
-        rightDown = 1 + doit(currx+1 , curry+1 , endx , endy , grid , grid[currx][curry]);
-        rightUp = 1 + doit(currx-1 , curry+1 , endx , endy , grid , grid[currx][curry]);
-        right = 1 + doit(currx , curry+1 , endx , endy , grid , grid[currx][curry]);
-        return dp[currx][curry] = max(rightDown , max(rightUp , right));
+        if(dp[currx][curry] != -1) return dp[currx][curry];
+        if(curry+1 < grid[0].size() && grid[currx][curry+1] > grid[currx][curry]){
+            right = 1 + doit(currx , curry+1 , grid);
+        }
+        if(currx+1 < grid.size() && curry+1 < grid[0].size() && grid[currx+1][curry+1] > grid[currx][curry]){
+            rightDown = 1 + doit(currx+1 , curry+1 , grid);
+        }
+        if(currx-1 < grid.size() && curry+1 < grid[0].size() && grid[currx-1][curry+1] > grid[currx][curry]){
+            rightUp = 1 + doit(currx-1 , curry+1 , grid);
+        }
+        return dp[currx][curry] = max(right , max(rightDown , rightUp));
     }
     int maxMoves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int finalMaxi = 0;
+        int finalans = 0;
         memset(dp , -1 , sizeof(dp));
-            for(int i = 0 ; i<m ; i++){
-                int result = doit(i , 0 , m , n , grid , -1);
-                finalMaxi = max(finalMaxi , result-1);
-            }
-        return finalMaxi;
+        for(int i = 0 ; i<m ; i++){
+            int ans = doit(i , 0 , grid);
+            finalans = max(finalans , ans);
+        }
+        return finalans;
     }
 };
