408afc9ba721d4b49e5b6f1c5d37d7a8fa9b0c85
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-29 19:40:04 +0530

    Time: 36 ms (17.58%), Space: 67.1 MB (99.63%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int dp[1000][1000];
+    int prev[1000];
+    int curr[1000];
     int maxMoves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        for(int i = 0 ; i<m ; i++) dp[i][n-1] = 0;
+        for(int i = 0 ; i<m ; i++) prev[i] = 0;
 
         for(int j = n-2 ; j>=0 ; j--){
             for(int i = 0 ; i<m ; i++){
                 int right = 0;
                 int rightDown = 0;
                 int rightUp = 0;
-                if(j+1 < n && grid[i][j+1] > grid[i][j]) right = 1 + dp[i][j+1];
-                if(j+1 < n && i+1 < m && grid[i+1][j+1] > grid[i][j]) rightDown = 1 + dp[i+1][j+1];
-                if(i-1 >=0 && j+1 < n && grid[i-1][j+1] > grid[i][j]) rightUp = 1 + dp[i-1][j+1];
-                dp[i][j] = max(right , max(rightDown , rightUp));
+                if(j+1 < n && grid[i][j+1] > grid[i][j]) right = 1 + prev[i];
+                if(j+1 < n && i+1 < m && grid[i+1][j+1] > grid[i][j]) rightDown = 1 + prev[i+1];
+                if(i-1 >=0 && j+1 < n && grid[i-1][j+1] > grid[i][j]) rightUp = 1 + prev[i-1];
+                curr[i] = max(right , max(rightDown , rightUp));
             }
+            swap(prev , curr);
         }
         int maxi = 0;
         for(int i = 0 ; i<m ; i++){
-            maxi = max(maxi , dp[i][0]);
+            maxi = max(maxi , prev[i]);
         }
         return maxi;
     }
