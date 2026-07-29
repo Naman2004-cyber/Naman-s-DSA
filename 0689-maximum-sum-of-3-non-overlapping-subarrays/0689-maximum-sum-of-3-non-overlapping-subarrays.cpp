ae33a3f32b002e8dfc12e24646b218df4dd48cd9
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-29 23:19:52 +0530

    Time: 9 ms (41.96%), Space: 26.7 MB (67.24%) - LeetHub

---
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> mpp(nums.size());
+        vector<int> subarraySum(nums.size());
+        int sum = 0;
+        for(int i = n-1 ; i>=0 ; i--){
+            sum+=nums[i];
+            mpp[i] = sum;
+            if(i+k-1 < n){
+                int prefSum = sum;
+                if(i+k < n) prefSum-=mpp[i+k];
+                subarraySum[i] = prefSum;
+            }
+            else{
+                subarraySum[i] = 0;
+            }
+        }
+        // for(int i = 0 ; i<n ; i++) cout << subarraySum[i] << " ";
+        // cout << endl;
+
+        int maxSumRight = 0;
+        int maxIndexRight = -1;
+        vector<int> maxIndexRightArray(nums.size());
+        for(int i = n-1 ; i>=0 ; i--){
+            if(subarraySum[i] >= maxSumRight){
+                maxSumRight = subarraySum[i];
+                maxIndexRight = i;
+            }
+            maxIndexRightArray[i] = maxIndexRight;
+        }
+        // for(int i = 0 ; i<n ; i++) cout << maxIndexRightArray[i] << " ";
+        // cout << endl;
+
+        int maxSumLeft = 0;
+        int maxIndexLeft = -1;
+        vector<int> maxIndexLeftArray(nums.size());
+        for(int i = 0 ; i<n ; i++){
+            if(subarraySum[i] > maxSumLeft){
+                maxSumLeft = subarraySum[i];
+                maxIndexLeft = i;
+            }
+            maxIndexLeftArray[i] = maxIndexLeft;
+        }
+        // for(int i = 0 ; i<n ; i++) cout << maxIndexLeftArray[i] << " ";
+        // cout << endl;
+        vector<int> ans;
+        int maxAns = 0;
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(i == n-1 || i-k < 0 || i+k >= n) continue;
+            int bestRight = maxIndexRightArray[i+k];
+            int bestLeft = maxIndexLeftArray[i-k];
+            if(subarraySum[i] > 0 && subarraySum[bestRight] > 0 && subarraySum[bestLeft] > 0){
+                int sum = subarraySum[i] + subarraySum[bestRight] + subarraySum[bestLeft];
+                if(sum > maxAns){
+                    maxAns = sum;
+                    ans = {bestLeft , i , bestRight};
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
