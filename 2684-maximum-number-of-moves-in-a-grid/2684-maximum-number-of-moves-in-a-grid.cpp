a543c4de2c4716c330320c38c4c1da3ab6377bc0
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-29 19:34:58 +0530

    Time: 85 ms (5.68%), Space: 71.6 MB (87.73%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int dp[1000][1000];
-    int doit(int currx , int curry , vector<vector<int>>& grid){
-        int right = 0;
-        int rightDown = 0;
-        int rightUp = 0;
-        if(dp[currx][curry] != -1) return dp[currx][curry];
-        if(curry+1 < grid[0].size() && grid[currx][curry+1] > grid[currx][curry]){
-            right = 1 + doit(currx , curry+1 , grid);
-        }
-        if(currx+1 < grid.size() && curry+1 < grid[0].size() && grid[currx+1][curry+1] > grid[currx][curry]){
-            rightDown = 1 + doit(currx+1 , curry+1 , grid);
-        }
-        if(currx-1 >=0 && curry+1 < grid[0].size() && grid[currx-1][curry+1] > grid[currx][curry]){
-            rightUp = 1 + doit(currx-1 , curry+1 , grid);
-        }
-        return dp[currx][curry] = max(right , max(rightDown , rightUp));
-    }
     int maxMoves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int finalans = 0;
-        memset(dp , -1 , sizeof(dp));
-        for(int i = 0 ; i<m ; i++){
-            int ans = doit(i , 0 , grid);
-            finalans = max(finalans , ans);
+
+        for(int i = 0 ; i<m ; i++) dp[i][n-1] = 0;
+
+        for(int j = n-2 ; j>=0 ; j--){
+            for(int i = 0 ; i<m ; i++){
+                int right = 0;
+                int rightDown = 0;
+                int rightUp = 0;
+                if(j+1 < n && grid[i][j+1] > grid[i][j]) right = 1 + dp[i][j+1];
+                if(j+1 < n && i+1 < m && grid[i+1][j+1] > grid[i][j]) rightDown = 1 + dp[i+1][j+1];
+                if(i-1 >=0 && j+1 < n && grid[i-1][j+1] > grid[i][j]) rightUp = 1 + dp[i-1][j+1];
+                dp[i][j] = max(right , max(rightDown , rightUp));
+            }
         }
-        return finalans;
+        int maxi = 0;
+        for(int i = 0 ; i<m ; i++){
+            maxi = max(maxi , dp[i][0]);
+        }
+        return maxi;
     }
 };
