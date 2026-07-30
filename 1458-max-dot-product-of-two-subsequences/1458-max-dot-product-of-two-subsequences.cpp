223d773155880c96d4bcaa505bb1ba23e58035c9
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-30 19:39:45 +0530

    Time: 27 ms (40.37%), Space: 15.5 MB (88.27%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -7,11 +7,11 @@ public:
             return INT_MIN;
         }
         if(dp[idx1][idx2][started] != -1) return dp[idx1][idx2][started];
-        int skipBoth = doit(idx1+1 , idx2+1 , nums1 , nums2 , started);
+        // int skipBoth = doit(idx1+1 , idx2+1 , nums1 , nums2 , started);
         int skip1 = doit(idx1+1 , idx2 , nums1 , nums2 , started);
         int skip2 = doit(idx1 , idx2+1 , nums1 , nums2 , started);
         int take = nums1[idx1]*nums2[idx2] + doit(idx1+1 , idx2+1 , nums1 , nums2 , 1);
-        return dp[idx1][idx2][started] = max(max(skipBoth , skip1) , max(skip2 , take));
+        return dp[idx1][idx2][started] = max(max(skip1 , skip2) , take);
     }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         memset(dp , -1 , sizeof(dp));
