9d22a1d0540020cc226a51bd9139efd28933d591
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-30 23:10:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (86.49%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> lastSeen(26 , 0);
+        vector<int> ans;
+        for(int i = 0 ; i<s.size() ; i++) lastSeen[s[i]-'a'] = i;
+        int i = 0 , j = 0;
+        int maxi = INT_MIN;
+        while(i < s.size() && j < s.size()){
+            maxi = max(lastSeen[s[j]-'a'] , maxi);
+            if(lastSeen[s[j]-'a'] == j && lastSeen[s[j]-'a'] == maxi){
+                ans.push_back(j-i+1);
+                j++;
+                i = j;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
