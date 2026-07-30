18510a00d4875cd36ecf1bfa6489e9f162ccff72
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-30 19:06:13 +0530

    Time: 155 ms (60%), Space: 162.1 MB (62.13%) - LeetHub

--- 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
+++ 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int dp[100000][3][3];
+    int dp[100001][3][3];
     int mod = 1e9+7;
-    int doit(int idx , vector<int>& nums , int count_even , int count_odd){
-        if(idx == nums.size()) return 1;
-        if(dp[idx][count_even][count_odd] != -1) return dp[idx][count_even][count_odd];
-        int take = 0;
-        int skip = 0;
-        skip = (skip + doit(idx+1 , nums , count_even , count_odd))%mod;
-        if(nums[idx]%2 == 0 && count_even <= 1){
-            take = (take + doit(idx+1 , nums , count_even+1 , 0))%mod;
-        }
-        if(nums[idx]%2 != 0 && count_odd <= 1){
-            take = (take + doit(idx+1 , nums , 0 , count_odd+1))%mod;
-        }
-        return dp[idx][count_even][count_odd] = (take + skip)%mod;
-    }
     int countStableSubsequences(vector<int>& nums) {
-        memset(dp , -1 , sizeof(dp));
-        return doit(0 , nums , 0 , 0)-1;
+        int n = nums.size();
+        for(int i = 0 ; i<=2 ; i++){
+            for(int j = 0 ; j<=2 ; j++){
+                dp[n][i][j] = 1;
+            }
+        }
+        for(int idx = n-1 ; idx>=0 ; idx--){
+            for(int count_even = 0 ; count_even <=2 ; count_even++){
+                for(int count_odd = 0 ; count_odd <= 2 ; count_odd++){
+                    int take = 0;
+                    int skip = 0;
+                    skip = skip + dp[idx+1][count_even][count_odd];
+                    if(nums[idx]%2 == 0 && count_even <= 1){
+                        take = (take + dp[idx+1][count_even+1][0])%mod;
+                    }
+                    if(nums[idx]%2 != 0 && count_odd <= 1){
+                        take = (take + dp[idx+1][0][count_odd+1])%mod;
+                    }
+                    dp[idx][count_even][count_odd] = (take+skip)%mod;
+                }
+            }
+        }
+        return dp[0][0][0]-1;
     }
 };
