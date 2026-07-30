a00a98efff0b238ca1bf1f549edfced55cc5a066
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-30 21:51:52 +0530

    Time: 15 ms (73.95%), Space: 14.5 MB (90.62%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int dp[500][500][2];
-    int doit(int idx1 , int idx2 , vector<int>& nums1 , vector<int>& nums2 , int started){
+    int dp[500][500];
+    int doit(int idx1 , int idx2 , vector<int>& nums1 , vector<int>& nums2){
         if(idx1 == nums1.size() || idx2 == nums2.size()){
-            if(started) return 0;
             return INT_MIN;
         }
-        if(dp[idx1][idx2][started] != -1) return dp[idx1][idx2][started];
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
         // int skipBoth = doit(idx1+1 , idx2+1 , nums1 , nums2 , started);
-        int skip1 = doit(idx1+1 , idx2 , nums1 , nums2 , started);
-        int skip2 = doit(idx1 , idx2+1 , nums1 , nums2 , started);
-        int take = nums1[idx1]*nums2[idx2] + doit(idx1+1 , idx2+1 , nums1 , nums2 , 1);
-        return dp[idx1][idx2][started] = max(max(skip1 , skip2) , take);
+        int skip1 = doit(idx1+1 , idx2 , nums1 , nums2);
+        int skip2 = doit(idx1 , idx2+1 , nums1 , nums2);
+        int take = nums1[idx1]*nums2[idx2] + max(0 , doit(idx1+1 , idx2+1 , nums1 , nums2));
+        return dp[idx1][idx2] = max(max(skip1 , skip2) , take);
     }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         memset(dp , -1 , sizeof(dp));
-        return doit(0 , 0 , nums1 , nums2 , 0);
+        return doit(0 , 0 , nums1 , nums2);
     }
 };
