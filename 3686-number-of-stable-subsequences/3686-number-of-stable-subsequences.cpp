565e03b7ca2d6f09463833b657fc9947f42ef373
Naman2004-cyber <namaniit2004@gmail.com>
2026-07-30 19:14:45 +0530

    Time: 75 ms (62.55%), Space: 158.2 MB (82.55%) - LeetHub

--- 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
+++ 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int dp[100001][3][3];
+    int prev[3][3];
+    int curr[3][3];
     int mod = 1e9+7;
     int countStableSubsequences(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0 ; i<=2 ; i++){
             for(int j = 0 ; j<=2 ; j++){
-                dp[n][i][j] = 1;
+                prev[i][j] = 1;
             }
         }
         for(int idx = n-1 ; idx>=0 ; idx--){
@@ -14,17 +15,18 @@ public:
                 for(int count_odd = 0 ; count_odd <= 2 ; count_odd++){
                     int take = 0;
                     int skip = 0;
-                    skip = skip + dp[idx+1][count_even][count_odd];
+                    skip = skip + prev[count_even][count_odd];
                     if(nums[idx]%2 == 0 && count_even <= 1){
-                        take = (take + dp[idx+1][count_even+1][0])%mod;
+                        take = (take + prev[count_even+1][0])%mod;
                     }
                     if(nums[idx]%2 != 0 && count_odd <= 1){
-                        take = (take + dp[idx+1][0][count_odd+1])%mod;
+                        take = (take + prev[0][count_odd+1])%mod;
                     }
-                    dp[idx][count_even][count_odd] = (take+skip)%mod;
+                    curr[count_even][count_odd] = (take+skip)%mod;
                 }
             }
+            swap(prev , curr);
         }
-        return dp[0][0][0]-1;
+        return prev[0][0]-1;
     }
 };
