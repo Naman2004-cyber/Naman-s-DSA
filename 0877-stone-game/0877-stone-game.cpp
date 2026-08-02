d9ad5e4db622088d11548b0ded54eddad7bd4924
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-02 14:05:45 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        int total = 0;
+        int firstHalf = 0;
+        int oneSkip = 0;
+        for(int i = 0 ; i<n ; i++){
+            if(i < (n/2)) firstHalf+=piles[i];
+            if(i%2 == 0) oneSkip+=piles[i];
+            total+=piles[i];
+        }
+        int bobsecondHalf = total - firstHalf;
+        if(firstHalf != bobsecondHalf) return true;
+        int leftSkip = total - oneSkip;
+        if(oneSkip != leftSkip) return true;
+        return false;
     }
 };
