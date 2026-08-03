07096f3575f0b6943a0c0198bf237cb6abea9183
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-03 21:22:15 +0530

    Time: 9 ms (31.18%), Space: 11.4 MB (38.63%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
+    int dp[500][500];
+    int doit(int startIdx , int endIdx , vector<int>& piles){
+        if(endIdx < startIdx) return 0; 
+        if(dp[startIdx][endIdx] != -1) return dp[startIdx][endIdx];
+        int selectStart = piles[startIdx] + min(doit(startIdx+2 , endIdx , piles) , doit(startIdx+1 , endIdx-1 , piles));
+        int selectEnd = piles[endIdx] + min(doit(startIdx , endIdx-2 , piles) , doit(startIdx+1 , endIdx-1 , piles));
+        return dp[startIdx][endIdx]= max(selectStart , selectEnd);
+    }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        int total = 0;
-        int firstHalf = 0;
-        int oneSkip = 0;
-        for(int i = 0 ; i<n ; i++){
-            if(i < (n/2)) firstHalf+=piles[i];
-            if(i%2 == 0) oneSkip+=piles[i];
-            total+=piles[i];
-        }
-        int bobsecondHalf = total - firstHalf;
-        if(firstHalf != bobsecondHalf) return true;
-        int leftSkip = total - oneSkip;
-        if(oneSkip != leftSkip) return true;
+        int total = accumulate(piles.begin() , piles.end() , 0);
+        memset(dp , -1 , sizeof(dp));
+        int aliceScore = doit(0 , n-1 , piles);
+        int bobScore = total - aliceScore;
+        if(aliceScore > bobScore) return true;
         return false;
     }
 };
