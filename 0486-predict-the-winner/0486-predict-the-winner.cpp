b22c03d7573e5e57679a33b77016a3a87f875a22
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-03 09:50:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (95.73%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -4,16 +4,15 @@ public:
     int doit(int startIdx , int endIdx , vector<int>& nums){
         if(startIdx > endIdx) return 0;
         if(dp[startIdx][endIdx] != -1) return dp[startIdx][endIdx];
-        int startSe = nums[startIdx] + min(doit(startIdx+2 , endIdx , nums) , doit(startIdx+1 , endIdx-1 , nums));
-        int endSe = nums[endIdx] + min(doit(startIdx , endIdx-2 , nums) , doit(startIdx+1 , endIdx-1 , nums));
+        int startSe = nums[startIdx] - doit(startIdx+1 , endIdx , nums);
+        int endSe = nums[endIdx] - doit(startIdx , endIdx-1 , nums);
         return dp[startIdx][endIdx] = max(startSe , endSe);
     }
     bool predictTheWinner(vector<int>& nums) {
         int total = accumulate(nums.begin() , nums.end() , 0);
         memset(dp , -1 , sizeof(dp));
-        int player1Sum = doit(0 , nums.size()-1 , nums);
-        cout << player1Sum;
-        if(player1Sum >= (total - player1Sum)) return true;
+        int ans = doit(0 , nums.size()-1 , nums);
+        if(ans >= 0) return true;
         return false;
     }
 };
