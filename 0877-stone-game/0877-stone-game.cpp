8423b17829289ec85f765723b3742ad2adbca2d3
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-03 21:40:17 +0530

    Time: 5 ms (37.57%), Space: 11.6 MB (37.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int dp[500][500];
-    int doit(int startIdx , int endIdx , vector<int>& piles){
-        if(endIdx < startIdx) return 0; 
-        if(dp[startIdx][endIdx] != -1) return dp[startIdx][endIdx];
-        int selectStart = piles[startIdx] + min(doit(startIdx+2 , endIdx , piles) , doit(startIdx+1 , endIdx-1 , piles));
-        int selectEnd = piles[endIdx] + min(doit(startIdx , endIdx-2 , piles) , doit(startIdx+1 , endIdx-1 , piles));
-        return dp[startIdx][endIdx]= max(selectStart , selectEnd);
-    }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
         int total = accumulate(piles.begin() , piles.end() , 0);
-        memset(dp , -1 , sizeof(dp));
-        int aliceScore = doit(0 , n-1 , piles);
+        for(int i = 0 ; i<n ; i++){
+            dp[i][i] = piles[i];
+        }
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int j = 0 ; j<n ; j++){
+                if(j < i || i == j) continue;
+                int startFirstChoice = i+2<n ? dp[i+2][j] : INT_MAX;
+                int startSecondChoice = i+1<n && j-1>=0 ? dp[i+1][j-1] : INT_MAX;
+                int selectStart = piles[i] + min(startFirstChoice ,startSecondChoice);
+
+                int endFirstChoice = j-2>=0 ? dp[i][j-2] : INT_MAX;
+                int endSecondChoice = i+1 < n && j-1 >=0 ? dp[i+1][j-1] : INT_MAX;
+                int selectEnd = piles[j] + min(endFirstChoice , endSecondChoice);
+
+                dp[i][j] = max(selectStart , selectEnd);
+            }
+        }
+        int aliceScore = dp[0][n-1];
         int bobScore = total - aliceScore;
         if(aliceScore > bobScore) return true;
         return false;
