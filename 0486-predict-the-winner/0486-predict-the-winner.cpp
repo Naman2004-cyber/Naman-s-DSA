354e1632534d8aac60277821f9441e2aa9670c97
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-03 14:56:01 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.88%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int dp[20][20];
-    int doit(int startIdx , int endIdx , vector<int>& nums){
-        if(startIdx > endIdx) return 0;
-        if(dp[startIdx][endIdx] != -1) return dp[startIdx][endIdx];
-        int startSe = nums[startIdx] - doit(startIdx+1 , endIdx , nums);
-        int endSe = nums[endIdx] - doit(startIdx , endIdx-1 , nums);
-        return dp[startIdx][endIdx] = max(startSe , endSe);
-    }
     bool predictTheWinner(vector<int>& nums) {
-        int total = accumulate(nums.begin() , nums.end() , 0);
-        memset(dp , -1 , sizeof(dp));
-        int ans = doit(0 , nums.size()-1 , nums);
-        if(ans >= 0) return true;
+        int n = nums.size();
+        for(int i = 0 ; i<n ; i++){
+            for(int j = i-1 ; j>=0 ; j--){
+                if(j < i) dp[i][j] = 0;
+            }
+        }
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int j = 0 ; j<n ; j++){
+                if(j < i) continue;
+                int startSe = 0;
+                if(i+1 < n) startSe = nums[i] - dp[i+1][j];
+                int endSe = 0;
+                if(j-1 >= 0) endSe = nums[j] - dp[i][j-1];
+                dp[i][j] = max(startSe , endSe);
+            }
+        }
+        if(dp[0][n-1] >= 0) return true;
         return false;
     }
 };
