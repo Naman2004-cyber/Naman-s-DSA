d41b7bc98ba2d1379f4cd5670f3c8ec035d6aff5
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-04 00:09:41 +0530

    Time: 0 ms (100%), Space: 136.2 MB (76.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int doit(int startIdx , vector<int>& stoneValue , vector<int>& dp){
-        if(startIdx >= stoneValue.size()) return 0;
-        if(dp[startIdx] != INT_MIN) return dp[startIdx];
-        int one = stoneValue[startIdx] - doit(startIdx+1 , stoneValue , dp);
-        int two = -2001;
-        if(startIdx+1 < stoneValue.size()){
-            two = stoneValue[startIdx] + stoneValue[startIdx+1] - doit(startIdx+2 , stoneValue , dp);
-        }
-        int three = -2001;
-        if(startIdx+2 < stoneValue.size()){
-            three  = stoneValue[startIdx] + stoneValue[startIdx+1] + stoneValue[startIdx+2] - doit(startIdx+3 , stoneValue , dp);
-        }
-        return dp[startIdx] = max(one , max(two , three));
-    }
     string stoneGameIII(vector<int>& stoneValue) {
-        vector<int> dp(stoneValue.size() , INT_MIN);
-        int bestDiff = doit(0 , stoneValue , dp);
+        int n = stoneValue.size();
+        vector<int> dp(stoneValue.size()+1);
+        dp[n] = 0;
+        for(int i = n-1 ; i>=0 ; i--){
+            int one = stoneValue[i] - dp[i+1];
+            int two = -2001;
+            if(i+1 < stoneValue.size()){
+                two = stoneValue[i] + stoneValue[i+1] - dp[i+2];
+            }
+            int three = -2001;
+            if(i+2 < stoneValue.size()){
+                three = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3];
+            }
+            dp[i] = max(one , max(two , three));
+        }
+        int bestDiff = dp[0];
         if(bestDiff > 0) return "Alice";
         if(bestDiff < 0) return "Bob";
         return "Tie";
