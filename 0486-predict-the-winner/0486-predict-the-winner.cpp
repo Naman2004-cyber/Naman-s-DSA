1b5795de93b1dc5dd2c034a5fd032331053ebe82
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-03 15:11:11 +0530

    Time: 3 ms (33.86%), Space: 9.5 MB (95.73%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int curr[20];
-    int prev[20];
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0 ; i<n ; i++){
             for(int j = i ; j>=0 ; j--){
-                if(j == i) prev[j] = nums[i];
+                if(j == i) curr[j] = nums[i];
             }
         }
         for(int i = n-1 ; i>=0 ; i--){
-            for(int j = 0 ; j<n ; j++){
-                if(j < i) continue;
+            for(int j = i+1 ; j<n ; j++){
+                // if(j < i) continue;
                 int startSe = 0;
-                if(i+1 < n) startSe = nums[i] - prev[j];
+                if(i+1 < n) startSe = nums[i] - curr[j];
                 int endSe = 0;
                 if(j-1 >= 0) endSe = nums[j] - curr[j-1];
                 curr[j] = max(startSe , endSe);
             }
-            swap(curr , prev);
         }
-        if(prev[n-1] >= 0) return true;
+        if(curr[n-1] >= 0) return true;
         return false;
     }
 };
