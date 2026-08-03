b7b9e013479c80124eaddd976c7c6e8fda849192
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-03 09:32:36 +0530

    Time: 0 ms (100%), Space: 9.5 MB (84.07%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int dp[20][20];
     int doit(int startIdx , int endIdx , vector<int>& nums){
         if(startIdx > endIdx) return 0;
+        if(dp[startIdx][endIdx] != -1) return dp[startIdx][endIdx];
         int startSe = nums[startIdx] + min(doit(startIdx+2 , endIdx , nums) , doit(startIdx+1 , endIdx-1 , nums));
         int endSe = nums[endIdx] + min(doit(startIdx , endIdx-2 , nums) , doit(startIdx+1 , endIdx-1 , nums));
-        return max(startSe , endSe);
+        return dp[startIdx][endIdx] = max(startSe , endSe);
     }
     bool predictTheWinner(vector<int>& nums) {
         int total = accumulate(nums.begin() , nums.end() , 0);
+        memset(dp , -1 , sizeof(dp));
         int player1Sum = doit(0 , nums.size()-1 , nums);
         cout << player1Sum;
         if(player1Sum >= (total - player1Sum)) return true;
