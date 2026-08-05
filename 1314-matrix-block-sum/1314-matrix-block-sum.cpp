ae5a6b6e63bb2d68cb641b4489e7c4594e5002a1
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-05 20:53:45 +0530

    Time: 1 ms (45.29%), Space: 13.5 MB (87.78%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int m = mat.size();
-        int n = mat[0].size();
-        for(int i = 0 ; i<m ; i++){
-            for(int j = 1 ; j<n ; j++){
-                mat[i][j]+=mat[i][j-1];
-            }
-        }
-        for(int j = 0 ; j<n ; j++){
-            for(int i = 1 ; i<m ; i++){
-                mat[i][j]+=mat[i-1][j];
-            }
-        }
+        int m = mat.size() , n = mat[0].size();
+        for(int i = 0 ; i<m ; i++) for(int j = 1 ; j<n ; j++) mat[i][j]+=mat[i][j-1];
+        for(int j = 0 ; j<n ; j++) for(int i = 1 ; i<m ; i++) mat[i][j]+=mat[i-1][j];
         vector<vector<int>> ans(m , vector<int>(n , 0));
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
