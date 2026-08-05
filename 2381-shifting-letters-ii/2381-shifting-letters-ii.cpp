6373d716971eeea56d902879ea625de469e8eccf
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-05 18:14:16 +0530

    Time: 13 ms (32.95%), Space: 97.9 MB (53.94%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -4,9 +4,7 @@ public:
         int n = s.size();
         vector<int> prefix(n);
         for(int i = 0 ; i<shifts.size() ; i++){
-            int l = shifts[i][0];
-            int r = shifts[i][1];
-            int dir = shifts[i][2];
+            int l = shifts[i][0] , r = shifts[i][1] , dir = shifts[i][2];
             if(dir == 1){
                 prefix[l]+=1;
                 if(r+1 < n) prefix[r+1]-=1;
@@ -16,27 +14,16 @@ public:
                 if(r+1 < n) prefix[r+1]+=1;
             }
         }
-        for(int i = 1 ; i<n ; i++){
-            prefix[i]+=prefix[i-1];
-        }
+        for(int i = 1 ; i<n ; i++) prefix[i]+=prefix[i-1];
         for(int i = 0 ; i<n ; i++){
             if(prefix[i] >= 0){
-                int naya = prefix[i]%26;
-                int charVal = s[i] + naya;
-                if(charVal > 122){
-                    int diff = charVal - 122;
-                    s[i] = 'a' + diff - 1;
-                }
+                int naya = prefix[i]%26 , charVal = s[i] + naya;
+                if(charVal > 122) s[i] = 'a' + (charVal - 122) - 1;
                 else s[i]+=naya;
             }
             else{
-                int naya = abs(prefix[i]);
-                naya = naya%26;
-                int charVal = s[i] - naya;
-                if(charVal < 97){
-                    int diff = 97 - charVal;
-                    s[i] = 'z' - diff + 1;
-                }
+                int naya = abs(prefix[i])%26 , charVal = s[i] - naya;
+                if(charVal < 97) s[i] = 'z' - (97 - charVal) + 1;
                 else s[i]-=naya;
             }
         }
