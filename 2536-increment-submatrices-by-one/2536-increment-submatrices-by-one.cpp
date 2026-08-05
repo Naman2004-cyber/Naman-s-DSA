d3e54d5184ac0ad4d6de0d9892b7731f3699e2cb
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-05 19:10:03 +0530

    Time: 15 ms (92.52%), Space: 99.3 MB (74.58%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -4,14 +4,19 @@ public:
         vector<vector<int>> diff(n , vector<int>(n , 0));
         for(int i = 0 ; i<queries.size() ; i++){
             int sX = queries[i][0] , sY = queries[i][1] , eX = queries[i][2] , eY = queries[i][3];
-            for(int row = sX ; row <= eX ; row++){
-                diff[row][sY]+=1;
-                if(eY+1 < n) diff[row][eY+1]-=1;
-            }
+            diff[sX][sY]++;
+            if(eY+1 < n) diff[sX][eY+1]--;
+            if(eX+1 < n) diff[eX+1][sY]--;
+            if(eX+1 < n && eY+1 < n) diff[eX+1][eY+1]++;
         }
         for(int i = 0 ; i<n ; i++){
             for(int j = 1 ; j<n ; j++){
                 diff[i][j]+=diff[i][j-1];
+            }
+        }
+        for(int j = 0 ; j<n ; j++){
+            for(int i = 1 ; i<n ; i++){
+                diff[i][j]+=diff[i-1][j];
             }
         }
         return diff;
