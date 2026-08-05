483893f3372a992a21552b04026efed24a05e912
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-05 18:11:58 +0530

    Time: 0 ms (100%), Space: 98 MB (53.94%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
-        vector<int> diff(n);
+        vector<int> prefix(n);
         for(int i = 0 ; i<shifts.size() ; i++){
             int l = shifts[i][0];
             int r = shifts[i][1];
             int dir = shifts[i][2];
             if(dir == 1){
-                diff[l]+=1;
-                if(r+1 < n) diff[r+1]-=1;
+                prefix[l]+=1;
+                if(r+1 < n) prefix[r+1]-=1;
             }
             else{
-                diff[l]-=1;
-                if(r+1 < n) diff[r+1]+=1;
+                prefix[l]-=1;
+                if(r+1 < n) prefix[r+1]+=1;
             }
         }
-        vector<int> prefix(n);
-        prefix[0] = diff[0];
         for(int i = 1 ; i<n ; i++){
-            prefix[i] = prefix[i-1] + diff[i];
+            prefix[i]+=prefix[i-1];
         }
         for(int i = 0 ; i<n ; i++){
             if(prefix[i] >= 0){
