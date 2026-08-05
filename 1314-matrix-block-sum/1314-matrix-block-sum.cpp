cc0fe9450a529340ec9cc90c6a26ce89b2096ff3
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-05 20:52:47 +0530

    Time: 0 ms (100%), Space: 13.7 MB (77.76%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -16,21 +16,11 @@ public:
         vector<vector<int>> ans(m , vector<int>(n , 0));
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
-                int sX = i-k >=0 ? i-k : 0;
-                int sY = j-k >=0 ? j-k : 0;
-                int eX = i+k < m ? i+k : m-1;
-                int eY = j+k < n ? j+k : n-1;
+                int sX = max(i-k , 0),sY = max(j-k , 0),eX = min(i+k , m-1) ,eY = min(j+k , n-1);
                 int total = mat[eX][eY];
-                int count = 0;
-                if(sX-1 >=0){
-                    total-=mat[sX-1][eY];
-                    count++;
-                }
-                if(sY-1 >=0){
-                    total-=mat[eX][sY-1];
-                    count++;
-                }
-                if(count == 2) total+=mat[sX-1][sY-1];
+                if(sX-1 >=0) total-=mat[sX-1][eY];
+                if(sY-1 >=0) total-=mat[eX][sY-1];
+                if(sX > 0 && sY > 0) total+=mat[sX-1][sY-1];
                 ans[i][j] = total;
             }
         }
