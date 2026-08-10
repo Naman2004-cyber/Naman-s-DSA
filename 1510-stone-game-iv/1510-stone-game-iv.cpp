c33f564f771f8524b2c2575972762ff4ac8f43d0
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-10 17:46:05 +0530

    Time: 140 ms (20.77%), Space: 15.3 MB (50.96%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    bool winnerSquareGame(int n) {
-        vector<int> dp(n+1);
-        dp[0] = -1;
-        for(int i = 1 ; i<=n ; i++){
-            int use = 1;
-            int flag = 0;
-            while(use*use <= i){
-                int val = dp[i-(use*use)];
-                if(val == -1) {
-                    dp[i] = 1;
-                    flag = 1;
-                    break;
-                }
-                use++;
-            }
-            if(flag == 0) dp[i] = -1;
+    int dp[100000];
+    bool doit(int n){
+        if(n == 0) return false;
+        if(dp[n] != -1) return dp[n];
+        int val = 1;
+        int flag = 0;
+        while(val*val <= n){
+            bool ans = doit(n - val*val);
+            if(!ans) flag = 1;
+            val++;
         }
-        int ans = dp[n];
-        if(ans == -1) return false;
-        return true;
+        if(flag == 1) return dp[n] = 1;
+        return dp[n] = 0;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp , -1 , sizeof(dp));
+        return doit(n);
     }
 };
