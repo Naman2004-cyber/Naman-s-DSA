e0a1a8e011e08701459db11a780e7d11164e2d45
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-10 20:21:45 +0530

    Time: 16 ms (93%), Space: 11.1 MB (63.7%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int dp[100000];
-    bool doit(int n){
-        if(n == 0) return false;
-        if(dp[n] != -1) return dp[n];
-        int val = 1;
-        int flag = 0;
-        while(val*val <= n){
-            bool ans = doit(n - val*val);
-            if(!ans) flag = 1;
-            val++;
-        }
-        if(flag == 1) return dp[n] = 1;
-        return dp[n] = 0;
-    }
+    // int dp[100000];
+    // bool doit(int n){
+    //     if(n == 0) return false;
+    //     if(dp[n] != -1) return dp[n];
+    //     int val = 1 , flag = 0;
+    //     while(val*val <= n){
+    //         bool ans = doit(n - val*val);
+    //         if(!ans) flag = 1;
+    //         val++;
+    //     }
+    //     if(flag == 1) return dp[n] = 1;
+    //     return dp[n] = 0;
+    // }
     bool winnerSquareGame(int n) {
-        memset(dp , -1 , sizeof(dp));
-        return doit(n);
+        vector<int> dp(n+1 , -1);
+        dp[1] = 1;
+        dp[0] = 0;
+        for(int i = 2 ; i<=n ; i++){
+            int flag = 0 , val = 1;
+            while(val*val <= i){
+                if(!dp[i - val*val]){
+                    flag = 1;
+                    break;
+                }
+                val++;
+            }
+            if(flag == 1) dp[i] = 1;
+            else dp[i] = 0;
+        }
+        return dp[n];
     }
 };
