5e24858d4ae109c2eff6122f6616637640b4147f
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-10 16:58:45 +0530

    Time: 147 ms (100%), Space: 247.2 MB (100%) - LeetHub

--- 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.cpp
+++ 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     long long weightedSum(vector<int>& parent, vector<int>& nums) {
-        // first of all form the adjaceny list which contains the children of each node and the values of the children
         vector<vector<pair<int , int>>> adj(parent.size());
         for(int i = 1 ; i<parent.size() ; i++){
             adj[parent[i]].push_back({i , nums[i]});
         }
         int index = 0;
-        // for(auto &it : adj){
-        //     cout << index << " ---> ";
-        //     for(auto &a : it){
-        //         cout << a.first << "," << a.second << " ";
-        //     }
-        //     cout << endl;
-        //     index++;
-        // }
-        // find the height of the tree
         int height = 0;
         queue<int> qt;
         qt.push(0);
