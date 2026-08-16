7536aed9886b3c635052e426694818c3884c3138
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-16 14:56:02 +0530

    Time: 4 ms (51.21%), Space: 178.6 MB (64.52%) - LeetHub

--- 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int minPenalty(int period, vector<int>& lights, vector<int>& arrivalTime) {
         int maxi = 0;
-        sort(lights.begin() , lights.end());
+        int maxLight = lights[0];
+        for(int i = 1 ; i<lights.size() ; i++) maxLight = max(maxLight , lights[i]);
         for(int i = 0 ; i<arrivalTime.size() ; i++){
             int r = arrivalTime[i]%period;
-        int idx = upper_bound(lights.begin() , lights.end() , r) - lights.begin();
-            if(idx == lights.size()) maxi = max(maxi , period-r);
+            if(r >= maxLight) maxi = max(maxi , period-r);
         }
         return maxi;
     }
