94a7a753f359e4b0fa949c1efc757e0ce3915d60
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-19 19:48:53 +0530

    Time: 21 ms (74.83%), Space: 53.9 MB (93.41%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        vector<int> check(11 , 0);
+        int mask = 0;
         int count = 1 , ans = 0;
         sort(reservedSeats.begin() , reservedSeats.end());
         int curr = reservedSeats[0][0];
         for(int i = 0 ; i<reservedSeats.size() ; i++){
-            if(reservedSeats[i][0] == curr) check[reservedSeats[i][1]] = 1;
+            if(reservedSeats[i][0] == curr) mask |= (1 << reservedSeats[i][1]);
             else{
                 count++;
-                int first = 1 , second = 1 , third = 1;
-                if(check[2] == 1 || check[3] == 1 || check[4] == 1 || check[5] == 1) first = 0;
-                if(check[4] == 1 || check[5] == 1 || check[6] == 1 || check[7] == 1) second = 0;
-                if(check[6] == 1 || check[7] == 1 || check[8] == 1 || check[9] == 1) third = 0;
+                int first = 0 , second = 0 , third = 0;
+                if((mask & (1<<2)) == 0 && (mask & (1<<3)) == 0 && (mask & (1<<4)) == 0 && (mask & (1<<5)) == 0) first = 1;
+                if((mask & (1<<4)) == 0 && (mask & (1<<5)) == 0 && (mask & (1<<6)) == 0 && (mask & (1<<7)) == 0) second = 1;
+                if((mask & (1<<6)) == 0 && (mask & (1<<7)) == 0 && (mask & (1<<8)) == 0 && (mask & (1<<9)) == 0) third = 1;
                 if(first == 1){
                     if(third == 1) ans+=2;
                     else ans++;
                 }
                 else if(second == 1 || third == 1) ans++;
                 curr = reservedSeats[i][0];
-                fill(check.begin() , check.end() , 0);
-                check[reservedSeats[i][1]] = 1;
+                mask = 0;
+                mask |= (1 << reservedSeats[i][1]);
             }
         }
-        int first = 1 , second = 1 , third = 1;
-                if(check[2] == 1 || check[3] == 1 || check[4] == 1 || check[5] == 1) first = 0;
-                if(check[4] == 1 || check[5] == 1 || check[6] == 1 || check[7] == 1) second = 0;
-                if(check[6] == 1 || check[7] == 1 || check[8] == 1 || check[9] == 1) third = 0;
+        int first = 0 , second = 0 , third = 0;
+                if((mask & (1<<2)) == 0 && (mask & (1<<3)) == 0 && (mask & (1<<4)) == 0 && (mask & (1<<5)) == 0) first = 1;
+                if((mask & (1<<4)) == 0 && (mask & (1<<5)) == 0 && (mask & (1<<6)) == 0 && (mask & (1<<7)) == 0) second = 1;
+                if((mask & (1<<6)) == 0 && (mask & (1<<7)) == 0 && (mask & (1<<8)) == 0 && (mask & (1<<9)) == 0) third = 1;
                 if(first == 1){
                     if(third == 1) ans+=2;
                     else ans++;
