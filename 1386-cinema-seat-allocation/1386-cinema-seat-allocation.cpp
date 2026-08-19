2957f1ca31b693d868e98fd31d7e19ff5b6ea9fe
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-19 17:57:57 +0530

    Time: 38 ms (56.76%), Space: 78.1 MB (29.22%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int , set<int>> mpp;
+        for(int i = 0 ; i<reservedSeats.size() ; i++){
+            mpp[reservedSeats[i][0]].insert(reservedSeats[i][1]);
+        }
+        int count = 0;
+        for(auto &it : mpp){
+            // cout << it.first << " --> ";
+            int first = 1;
+            int second = 1;
+            int third = 1;
+            for(auto &a : it.second){
+                if(a == 2 || a == 3 || a == 4 || a == 5) first = 0;
+                if(a == 4 || a == 5 || a == 6 || a == 7) second = 0;
+                if(a == 6 || a == 7 || a == 8 || a == 9) third = 0;
+            }
+            if(first == 1){
+                if(third == 1) count+=2;
+                else count++;
+            }
+            else if(second == 1 || third == 1) count++;
+            // cout << endl;
+        }
+        return count + ((n - mpp.size())*2);
+    }
+};
