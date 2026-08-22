f43bb36e3b57f086e1b806e5bd6e79bff94c9b82
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-22 18:51:04 +0530

    Time: 326 ms (56.05%), Space: 190.8 MB (42.77%) - LeetHub

--- 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
+++ 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void updateTree(int segIdx , int l , int r , int rank , vector<int> &segTree){
+    void updateTree(int segIdx , int l , int r , int rank , vector<int>& segTree){
         if(l == r){
             segTree[segIdx]++;
             return;
@@ -10,67 +10,43 @@ public:
         else updateTree(2*segIdx+2 , mid+1 , r , rank , segTree);
         segTree[segIdx] = segTree[2*segIdx+1] + segTree[2*segIdx+2];
     }
-    int freqQuery(int segIdx , int l , int r , int targetRank , vector<int>& segTree){
-        if(targetRank >= r) return 0;
-        if(targetRank < l) return segTree[segIdx];
-        // if(l == r){
-        //     return segTree[segIdx];
-        // }
+    int query(int segIdx , int l , int r , int rank , vector<int>& segTree){
+        if(rank >= r) return 0;
+        if(rank < l) return segTree[segIdx];
         int mid = (l+r)/2;
-        int left = freqQuery(2*segIdx+1 , l , mid , targetRank , segTree);
-        int right  = freqQuery(2*segIdx+2 , mid+1 , r , targetRank , segTree);
-        return left+right;
+        return query(2*segIdx+1,l,mid,rank,segTree)+query(2*segIdx+2,mid+1,r,rank,segTree);
     }
     vector<int> resultArray(vector<int>& nums) {
-        unordered_map<int , int> mpp;
-        vector<int> rank = nums;
-        sort(rank.begin() , rank.end());
-        int useRank = 1;
-        for(int i= 0 ; i<rank.size() ; i++){
-            if(mpp.find(rank[i]) == mpp.end()){
-                mpp[rank[i]] = useRank;
-                useRank++;
-            }
+        vector<int> sorted = nums;
+        sort(sorted.begin() , sorted.end());
+        vector<int> rank;
+        rank.push_back(sorted[0]);
+        for(int i = 1 ; i<sorted.size() ; i++){
+            if(sorted[i] != sorted[i-1]) rank.push_back(sorted[i]);
         }
-        unordered_map<int , int> rankMpp;
-        for(int i = 0 ; i<nums.size() ; i++){
-            rank[i] = mpp[nums[i]];
-            rankMpp[rank[i]] = nums[i];
-        }
-        vector<int> segTree1(4*mpp.size());
-        vector<int> arr1;
-        arr1.push_back(rank[0]);
-        updateTree(0 , 1 , mpp.size() , rank[0] , segTree1);
-        vector<int> segTree2(4*mpp.size());
-        vector<int> arr2;
-        arr2.push_back(rank[1]);
-        updateTree(0 , 1 , mpp.size() , rank[1] , segTree2);
-        for(int i = 2 ; i<rank.size() ; i++){
-            int countArr1 = freqQuery(0 , 1 , mpp.size() , rank[i] , segTree1);
-            int countArr2 = freqQuery(0 , 1 , mpp.size() , rank[i] , segTree2);
-            if(countArr1 > countArr2){
-                arr1.push_back(rank[i]);
-                updateTree(0 , 1 , mpp.size() , rank[i] , segTree1);
-            }
-            else if(countArr1 < countArr2){
-                arr2.push_back(rank[i]);
-                updateTree(0 , 1 , mpp.size() , rank[i] , segTree2);
-            }
-            else if(arr1.size() < arr2.size()){
-                arr1.push_back(rank[i]);
-                updateTree(0 , 1 , mpp.size() , rank[i] , segTree1);
-            }
-            else if(arr1.size() > arr2.size()){
-                arr2.push_back(rank[i]);
-                updateTree(0 , 1 , mpp.size() , rank[i] , segTree2);
+        unordered_map<int,int> mpp;
+        for(int i = 0 ; i<rank.size() ; i++) mpp[rank[i]] = i+1;
+        vector<int> compressed(nums.size());
+        for(int i = 0 ; i<nums.size() ; i++) compressed[i] = mpp[nums[i]];
+        vector<int> arr1 , arr2 , segTree1(4*rank.size() , 0) , segTree2(4*rank.size() , 0);
+        arr1.push_back(compressed[0]);
+        updateTree(0,1,rank.size(),compressed[0],segTree1);
+        arr2.push_back(compressed[1]);
+        updateTree(0,1,rank.size(),compressed[1],segTree2);
+        for(int i = 2 ; i<compressed.size();i++){
+            int count1 = query(0,1,rank.size(),compressed[i],segTree1);
+            int count2 = query(0,1,rank.size(),compressed[i],segTree2);
+            if(count1 > count2 || ((count1 == count2) && (arr1.size() <= arr2.size()))){
+                arr1.push_back(compressed[i]);
+                updateTree(0,1,rank.size(),compressed[i],segTree1);
             }
             else{
-                arr1.push_back(rank[i]);
-                updateTree(0 , 1 , mpp.size() , rank[i] , segTree1);
+                arr2.push_back(compressed[i]);
+                updateTree(0,1,rank.size(),compressed[i],segTree2);
             }
         }
         for(int i = 0 ; i<arr2.size() ; i++) arr1.push_back(arr2[i]);
-        for(int i = 0 ; i<arr1.size() ; i++) arr1[i] = rankMpp[arr1[i]];
+        for(int i = 0 ; i<arr1.size() ; i++) arr1[i] = rank[arr1[i]-1];
         return arr1;
     }
 };
