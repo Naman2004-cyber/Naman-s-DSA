c69f3f7e6c7599b9c8a78e49343c91db02731478
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-23 18:15:50 +0530

    Time: 2051 ms (6.29%), Space: 14.1 MB (10.26%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,100 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int countLeft = 0; 
+        int countRight = 0;
+        int half = num.size()/2;
+        int leftSum = 0 , rightSum = 0;
+        for(int i = 0 ; i<num.size() ; i++){
+            if(i < half) {
+                if(num[i] == '?') countLeft++;
+                else leftSum+=(num[i]-'0');
+            }
+            else {
+                if(num[i] == '?') countRight++;
+                else rightSum+=(num[i]-'0');
+            }
+        }
+        cout << countLeft <<  " " << countRight << endl;
+        cout << leftSum << " " << rightSum << endl;
+        if(countLeft == 0 && countRight == 0){
+            if(leftSum != rightSum) return true;
+            return false;
+        }
+        int total = countLeft + countRight;
+        int turn = 0; // 0 means turn of alice and 1 means turn of bob
+        for(int i = 0 ; i<total ; i++){
+            if(turn == 0){
+                if(leftSum > rightSum){
+                    if(countLeft > 0) {
+                        leftSum+=9;
+                        countLeft--;
+                    }
+                    else if(countRight > 0) {
+                        int diff = leftSum - rightSum;
+                        if(diff < 9){
+                            rightSum+=9;
+                        }
+                        else rightSum+=0;
+                        countRight--;
+                    }
+                }
+                else if(rightSum > leftSum){
+                    if(countRight > 0){
+                        rightSum+=9;
+                        countRight--;
+                    }
+                    else if(countLeft > 0){
+                        int diff = rightSum - leftSum;
+                        if(diff < 9){
+                            leftSum+=9;
+                        }
+                        else leftSum+=0;
+                        countLeft--;
+                    }
+                }
+                else{
+                    if(countLeft >= countRight && countLeft > 0){
+                        leftSum+=9;
+                        countLeft--;
+                    }
+                    else if(countRight > 0){
+                        rightSum+=9;
+                        countRight--;
+                    }
+                }
+            }
+            else{
+                if(rightSum < leftSum){
+                    int diff = leftSum - rightSum;
+                    int valToAdd = diff > 9 ? 9 : diff;
+                    if(countRight > 0){
+                        rightSum+=valToAdd;
+                        countRight--;
+                    }
+                    else if(countLeft > 0){
+                        leftSum+=0;
+                        countLeft--;
+                    }
+                }
+                else if(leftSum < rightSum){
+                    int diff = rightSum - leftSum;
+                    int valToAdd = diff > 9 ? 9 : diff;
+                    if(countLeft > 0){
+                        leftSum+=valToAdd;
+                        countLeft--;
+                    }
+                    else if(countRight > 0){
+                        rightSum+=0;
+                        countRight--;
+                    }
+                }
+            }
+            turn = turn == 0 ? 1 : 0;
+            cout << leftSum << " " << rightSum << endl;
+        }
+        // cout << leftSum << " " << rightSum << endl;
+        if(leftSum == rightSum) return false;
+        return true;
+    }
+};
