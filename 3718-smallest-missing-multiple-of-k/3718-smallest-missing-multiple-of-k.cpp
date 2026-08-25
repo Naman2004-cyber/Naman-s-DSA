f7ae46b8c4de896fdce6e6bfd79da291e9ad133e
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-25 09:48:07 +0530

    Time: 0 ms (100%), Space: 24.5 MB (57.48%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         vector<int> freq(102 , 0);
-        for(int i = 0 ; i<nums.size() ; i++){
-            if(nums[i]%k == 0) freq[nums[i]/k]++;
-        } 
-        for(int i = 1 ; i<freq.size() ; i++){
-            if(freq[i] == 0) return i*k;
-        }
+        for(int i = 0 ; i<nums.size() ; i++) if(nums[i]%k == 0) freq[nums[i]/k]++;
+        for(int i = 1 ; i<freq.size() ; i++) if(freq[i] == 0) return i*k;
         return 0;
     }
 };
