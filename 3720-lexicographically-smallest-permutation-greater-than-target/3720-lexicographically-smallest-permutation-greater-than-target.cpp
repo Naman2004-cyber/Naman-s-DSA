87b86678d1b28e223ec44ec50921845885efcad3
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-28 23:36:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (98.21%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,74 +1,30 @@
 class Solution {
 public:
+    bool doit(int idx , string &ans , string &s , string &target , vector<int> &freq , int greater){
+        if(idx == s.size()){
+            if(greater == 1){
+                return true;
+            }
+            return false;
+        }
+        for(char ch = 'a' ; ch <= 'z' ; ch++){
+            if(freq[ch-'a'] == 0) continue;
+            if(greater == 0 && (ch < target[idx])) continue;
+            ans+=ch;
+            freq[ch-'a']--;
+            int newgreater = greater || (ch > target[idx]) ? 1 : 0;
+            if(doit(idx+1 , ans , s , target , freq , newgreater)) return true;
+            ans.pop_back();
+            freq[ch-'a']++;
+        }
+        return false;
+    }
     string lexGreaterPermutation(string s, string target) {
+        string ans = "";
         vector<int> freq(26 , 0);
         for(int i = 0 ; i<s.size() ; i++) freq[s[i]-'a']++;
-        string ans = "";
-        int placed = 0;
-        for(int i = 0 ; i<target.size() ; i++){
-            char ch = target[i];
-            if(placed == 0){
-                if(freq[target[i]-'a'] == 0){
-                    int flag = 0;
-                    for(int j = (ch-'a')+1 ; j<26 ; j++){
-                        if(freq[j] > 0){
-                            char use = 'a' + j;
-                            ans+=use;
-                            freq[j]--;
-                            flag = 1;
-                            if(use > target[i]) placed = 1;
-                            break;
-                        }
-                    }
-                    if(flag == 0) return "";
-                }
-                else{
-                    string use = "";
-                    for(int j = 25 ; j>=0 ; j--){
-                        if(j == target[i]-'a'){
-                            for(int k = 0 ; k<(freq[j]-1) ; k++){
-                                use+=(j+'a');
-                            }
-                        }
-                        else{
-                            for(int k = 0 ; k<freq[j] ; k++){
-                                use+=(j+'a');
-                            }
-                        }
-                    }
-                    string leftTarget = target.substr(i+1);
-                    if(use > leftTarget){
-                        ans+=target[i];
-                        freq[target[i]-'a']--;
-                    }
-                    else{
-                         int flag = 0;
-                        for(int j = (ch-'a')+1 ; j<26 ; j++){
-                            if(freq[j] > 0){
-                                char use = 'a' + j;
-                                ans+=use;
-                                freq[j]--;
-                                flag = 1;
-                                if(use > target[i]) placed = 1;
-                                break;
-                            }
-                        }
-                        if(flag == 0) return "";
-                    }
-                }
-            }
-            else{
-                for(int j = 0 ; j<26 ; j++){
-                    if(freq[j] > 0){
-                        char use = 'a' + j;
-                        ans+=use;
-                        freq[j]--;
-                        break;
-                    }
-                }
-            }
-        }
-        if(ans <= target) return "";
+        bool get = doit(0 , ans , s , target , freq , 0);
+        if(get ==  false) return "";
         return ans;
     }
 };
