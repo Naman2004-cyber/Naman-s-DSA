242a108d865b67e4d310481cd0bcf5228828f2bb
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-29 12:12:54 +0530

    Time: 379 ms (27.6%), Space: 224.8 MB (30.42%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -4,31 +4,22 @@ public:
         vector<int> sorted = nums;
         sort(sorted.begin() , sorted.end());
         vector<int> parent(nums.size());
-        for(int i = 0 ; i<nums.size() ; i++){
-            parent[i] = sorted[i];
-        }
+        for(int i = 0 ; i<nums.size() ; i++) parent[i] = sorted[i];
         for(int i = 0 ; i<nums.size()-1 ; i++){
-            if(sorted[i+1]-sorted[i] <= limit){
-                parent[i+1] = parent[i];
-            }
+            if(sorted[i+1]-sorted[i] <= limit) parent[i+1] = parent[i];
         }
-        // for(int i = 0 ; i<nums.size() ; i++) cout << parent[i] << " ";
         unordered_map<int , int> parentFind;
-        unordered_map<int , multiset<int>> mpp;
+        unordered_map<int , vector<int>> mpp;
         for(int i = 0 ; i<nums.size() ; i++){
-            mpp[parent[i]].insert(sorted[i]);
+            mpp[parent[i]].push_back(sorted[i]);
             parentFind[sorted[i]] = parent[i];
         }
-        // for(auto &it : mpp){
-        //     cout << it.first << " --> ";
-        //     for(auto &a : it.second) cout << a << " ";
-        //     cout << endl;
-        // }
+        unordered_map<int , int> ptr;
         for(int i = 0 ; i<nums.size() ; i++){
             int component = parentFind[nums[i]];
-            int x = *mpp[component].begin();
+            int x = mpp[component][ptr[component]];
             nums[i] = x;
-            mpp[component].erase(mpp[component].find(x));
+            ptr[component]++;
         }
         return nums;
     }
