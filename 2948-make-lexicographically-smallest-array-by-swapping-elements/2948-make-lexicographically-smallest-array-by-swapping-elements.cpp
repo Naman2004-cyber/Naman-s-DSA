388c7c88929c516a036a8f4c1ef1ac9593df2571
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-29 13:24:01 +0530

    Time: 352 ms (33.52%), Space: 224.9 MB (30.42%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -3,23 +3,23 @@ public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         vector<int> sorted = nums;
         sort(sorted.begin() , sorted.end());
-        vector<int> parent(nums.size());
-        for(int i = 0 ; i<nums.size() ; i++) parent[i] = sorted[i];
-        for(int i = 0 ; i<nums.size()-1 ; i++){
-            if(sorted[i+1]-sorted[i] <= limit) parent[i+1] = parent[i];
+        vector<int> parent = sorted;
+        parent[0] = sorted[0];
+        for(int i = 1 ; i<nums.size() ; i++){
+            if(sorted[i]-sorted[i-1] <= limit) parent[i] = parent[i-1];
         }
-        unordered_map<int , int> parentFind;
-        unordered_map<int , vector<int>> mpp;
+        unordered_map<int , int> findParent;
+        unordered_map<int , vector<int>> components;
         for(int i = 0 ; i<nums.size() ; i++){
-            mpp[parent[i]].push_back(sorted[i]);
-            parentFind[sorted[i]] = parent[i];
+            findParent[sorted[i]] = parent[i];
+            components[parent[i]].push_back(sorted[i]);
         }
         unordered_map<int , int> ptr;
         for(int i = 0 ; i<nums.size() ; i++){
-            int component = parentFind[nums[i]];
-            int x = mpp[component][ptr[component]];
+            int currComponent = findParent[nums[i]];
+            int x = components[currComponent][ptr[currComponent]];
             nums[i] = x;
-            ptr[component]++;
+            ptr[currComponent]++;
         }
         return nums;
     }
