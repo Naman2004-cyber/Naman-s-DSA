a7055ef030068a470034416ea300f8ea1da841c5
Naman2004-cyber <namaniit2004@gmail.com>
2026-08-31 11:19:54 +0530

    Time: 0 ms (100%), Space: 50.4 MB (62.5%) - LeetHub

--- 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.cpp
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minBishopMoves(vector<int>& source, vector<int>& target) {
         if(abs(source[0]-target[0]) == abs(source[1]-target[1])) return 1;
-        if(((source[0]+source[1])%2 == 0 && (target[0]+target[1])%2 == 0) || ((source[0]+source[1])%2 != 0 && (target[0]+target[1])%2 != 0)) return 2;
+        if((source[0]+source[1])%2 == (target[0]+target[1])%2) return 2;
         return -1;
     }
 };
