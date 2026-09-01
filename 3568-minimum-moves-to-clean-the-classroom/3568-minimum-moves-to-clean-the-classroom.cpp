f0b9057d9b9fcaa9002c01d86adb090f26025614
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-01 21:36:03 +0530

    Time: 1199 ms (39.8%), Space: 391.4 MB (34.69%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -3,67 +3,45 @@ public:
     int r[4] = {0 , 1 , 0 , -1};
     int c[4] = {1 , 0 , -1 , 0};
     int minMoves(vector<string>& classroom, int energy) {
-        int m = classroom.size();
-        int n = classroom[0].size();
-        int startx = -1 , starty = -1;
-        int countLitter = 0;
-        map<pair<int , int> , int> mpp;
+        int m = classroom.size() , n = classroom[0].size() , countLitter = 0 , startx = -1 , starty = -1;
+        vector<vector<int>> markLitter(m , vector<int>(n , -1));
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
                 if(classroom[i][j] == 'S'){
                     startx = i;
                     starty = j;
                 }
-                if(classroom[i][j] == 'L') {
-                    mpp[{i , j}] = countLitter;
-                    countLitter++;
-                }
+                if(classroom[i][j] == 'L') markLitter[i][j] = countLitter++;
             }
         }
-        queue<
-            tuple<
-                int , 
-                int ,
-                int ,
-                int 
-            >
-        > qt;
-        vector<vector<vector<vector<bool>>>> minCost(m+1 , vector<vector<vector<bool>>>(n+1 , vector<vector<bool>>(energy+1 , vector<bool>(1 << mpp.size() , false))));
-        qt.push({startx , starty , energy , 0}); 
-        minCost[startx][starty][energy][0] = true;
-        int allLitter = (1 << countLitter) - 1;
+        int allLittersMarkedMask = (1 << countLitter) - 1;
+        vector<vector<vector<vector<bool>>>> visited(m , vector<vector<vector<bool>>>(n , vector<vector<bool>>(energy+1 , vector<bool>(1 << countLitter , false))));
+        queue<tuple<int,int,int,int>> qt;
+        qt.push({startx , starty , energy , 0});
+        visited[startx][starty][energy][0] = true;
         int cost = 0;
         while(!qt.empty()){
-            int a = qt.size();
-            while(a != 0){
+            int s = qt.size();
+            for(int i = 0 ; i<s ; i++){
                 auto [currx , curry , currEnergy , currMask] = qt.front();
                 qt.pop();
-                if(currMask == allLitter) return cost;
-                if(currEnergy == 0){
-                    a--;
-                    continue;
-                }
-                for(int i = 0 ; i<4 ; i++){
-                    int newx = currx + r[i];
-                    int newy = curry + c[i];
-                    if(newx >= 0 && newx < m && newy >=0  && newy < n){
-                        if(classroom[newx][newy] == 'X') continue;
-                            int newEnergy = classroom[newx][newy] == 'R' ? energy : currEnergy-1;
-                            int newMask = currMask;
-                            if(classroom[newx][newy] == 'L'){
-                                int num = mpp[{newx , newy}];
-                                // check if it was marked or not;
-                                if((currMask & (1 << num)) == 0){
-                                    newMask = newMask | (1 << num);
-                                }
-                            }
-                            if(newEnergy == 0 && classroom[newx][newy] != 'L') continue;
-                            if(minCost[newx][newy][newEnergy][newMask]) continue;
-                            qt.push({newx , newy , newEnergy , newMask});
-                            minCost[newx][newy][newEnergy][newMask] = true;
+                if(currMask == allLittersMarkedMask) return cost;
+                if(currEnergy == 0) continue;
+                for(int j = 0 ; j<4 ; j++){
+                    int newx = currx + r[j] , newy = curry + c[j];
+                    if(newx < 0 || newy < 0 || newx >= m || newy >= n) continue;
+                    if(classroom[newx][newy] == 'X') continue;
+                    int newEnergy = classroom[newx][newy] == 'R' ? energy : currEnergy-1;
+                    if(newEnergy == 0 && classroom[newx][newy] != 'L') continue;
+                    int newMask = currMask;
+                    if(classroom[newx][newy] == 'L'){
+                        int bit = markLitter[newx][newy];
+                        newMask = newMask | (1 << bit);
                     }
+                    if(visited[newx][newy][newEnergy][newMask]) continue;
+                    qt.push({newx , newy , newEnergy , newMask});
+                    visited[newx][newy][newEnergy][newMask] = true;
                 }
-                a--;
             }
             cost++;
         }
