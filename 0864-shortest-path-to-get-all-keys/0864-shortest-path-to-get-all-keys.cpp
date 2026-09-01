8f7a71ea88474592a3aa8bdd65751b2bd1e09785
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-01 22:43:48 +0530

    Time: 15 ms (84.96%), Space: 14.7 MB (82.65%) - LeetHub

---
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int r[4] = {0 , 1 , 0 , -1} , c[4] = {1 , 0 , -1 , 0};
+    int shortestPathAllKeys(vector<string>& grid) {
+        int m = grid.size() , n = grid[0].size() , countKeys = 0 , startx = -1 ,starty = -1;
+        for(int i = 0 ; i<m ; i++) {
+            for(int j = 0 ; j<n ; j++) {
+                if(grid[i][j] >= 'a' && grid[i][j] <= 'z') countKeys++;
+                if(grid[i][j] == '@'){
+                    startx = i;
+                    starty = j;
+                }
+            }
+        }
+        int allKeysMask = (1 << countKeys) - 1;
+        queue<tuple<int,int,int>> qt;
+        vector<vector<vector<bool>>> visited(m , vector<vector<bool>>(n , vector<bool>(1<<countKeys , false)));
+        qt.push({startx , starty , 0});
+        visited[startx][starty][0] = true;
+        int moves = 0;
+        while(!qt.empty()){
+            int s = qt.size();
+            for(int i = 0 ; i<s ; i++){
+                auto [currx , curry , currMask] = qt.front();
+                qt.pop();
+                if(currMask == allKeysMask) return moves;
+                for(int j = 0 ; j<4 ; j++){
+                    int newx = currx + r[j] , newy = curry + c[j];
+                    if(newx < 0 || newy < 0 || newx >= m || newy >= n || grid[newx][newy] == '#') continue;
+                    if(grid[newx][newy] == '.'){
+                        if(visited[newx][newy][currMask]) continue;
+                        qt.push({newx , newy , currMask});
+                        visited[newx][newy][currMask] = true;
+                    }
+                    else if(grid[newx][newy] >= 'a' && grid[newx][newy] <= 'z'){
+                        int keyIndex = grid[newx][newy] - 'a';
+                        int newMask = (currMask | (1 << keyIndex));
+                        if(visited[newx][newy][newMask]) continue;
+                        qt.push({newx , newy , newMask});
+                        visited[newx][newy][newMask] = true;
+                    }
+                    else if(grid[newx][newy] >= 'A' && grid[newx][newy] <= 'Z'){
+                        int LockIndex = grid[newx][newy] - 'A';
+                        if((currMask & (1 << LockIndex)) == 0) continue;
+                        if(visited[newx][newy][currMask]) continue;
+                        qt.push({newx , newy , currMask});
+                        visited[newx][newy][currMask] = true;
+                    }
+                    else{
+                        if(visited[newx][newy][currMask]) continue;
+                        qt.push({newx , newy , currMask});
+                        visited[newx][newy][currMask] = true;
+                    }
+                }
+            }
+            moves++;
+        }
+        return -1;
+    }
+};
