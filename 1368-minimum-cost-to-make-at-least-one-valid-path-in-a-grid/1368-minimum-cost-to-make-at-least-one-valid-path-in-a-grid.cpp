9f49010a26b2c985696ebe3c721a3fb8f16dd910
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-02 18:55:59 +0530

    Time: 26 ms (44.04%), Space: 19.5 MB (64.88%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int r[4] = {0 , 1 , 0 , -1};
+    int c[4] = {1 , 0 , -1 , 0};
+    int minCost(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> minCost(m , vector<int>(n , INT_MAX));
+        priority_queue<tuple<int,int,int> , vector<tuple<int,int,int>> , greater<tuple<int,int,int>>> pq;
+        pq.push({0 , 0 , 0});
+        minCost[0][0] = 0;
+        while(!pq.empty()){
+            auto [currCost , currx , curry] = pq.top();
+            pq.pop();
+            if(currCost > minCost[currx][curry]) continue;
+            if(currx == m-1 && curry == n-1) return currCost;
+            int currDir = grid[currx][curry];
+            for(int i = 0 ; i<4 ; i++){
+                int newx = currx + r[i] , newy = curry + c[i];
+                if(newx < 0 || newy < 0 || newx >= m || newy >= n) continue;
+                // 0 is right , 1 is down , 2 is left and 3 is up
+                if((i == 0 && currDir == 1) || (i == 1 && currDir == 3) || (i == 2 && currDir == 2) || (i == 3 && currDir == 4)){
+                    if(currCost < minCost[newx][newy]){
+                        pq.push({currCost , newx , newy});
+                        minCost[newx][newy] = currCost;
+                    }
+                }
+                else{
+                    if(currCost+1 < minCost[newx][newy]){
+                        pq.push({currCost+1 , newx , newy});
+                        minCost[newx][newy] = currCost+ 1;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
