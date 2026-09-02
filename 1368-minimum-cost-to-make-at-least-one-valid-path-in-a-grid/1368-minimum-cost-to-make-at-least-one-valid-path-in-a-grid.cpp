3792dcc1bc226e8b4d43e29f0536cb095cbbf181
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-02 19:20:24 +0530

    Time: 11 ms (85.24%), Space: 18.8 MB (77.76%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int r[4] = {0 , 1 , 0 , -1};
-    int c[4] = {1 , 0 , -1 , 0};
+    int r[4] = {0 , 1 , 0 , -1} , c[4] = {1 , 0 , -1 , 0};
     int minCost(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m = grid.size() , n = grid[0].size();
         vector<vector<int>> minCost(m , vector<int>(n , INT_MAX));
-        priority_queue<tuple<int,int,int> , vector<tuple<int,int,int>> , greater<tuple<int,int,int>>> pq;
-        pq.push({0 , 0 , 0});
+        deque<pair<int , int>> dq;
         minCost[0][0] = 0;
-        while(!pq.empty()){
-            auto [currCost , currx , curry] = pq.top();
-            pq.pop();
-            if(currCost > minCost[currx][curry]) continue;
-            if(currx == m-1 && curry == n-1) return currCost;
+        dq.push_back({0 , 0});
+        while(!dq.empty()){
+            auto [currx , curry] = dq.front();
+            dq.pop_front();
+            if(currx == m-1 && curry == n-1) return minCost[currx][curry];
             int currDir = grid[currx][curry];
             for(int i = 0 ; i<4 ; i++){
                 int newx = currx + r[i] , newy = curry + c[i];
                 if(newx < 0 || newy < 0 || newx >= m || newy >= n) continue;
                 // 0 is right , 1 is down , 2 is left and 3 is up
-                int newCost = currCost;
-                if(!((i == 0 && currDir == 1) || (i == 1 && currDir == 3) || (i == 2 && currDir == 2) || (i == 3 && currDir == 4))) newCost++;
-                if(newCost < minCost[newx][newy]){
-                    pq.push({newCost, newx , newy});
-                    minCost[newx][newy] = newCost;
+                if((i == 0 && currDir == 1) || (i == 1 && currDir == 3) || (i == 2 && currDir == 2) || (i == 3 && currDir == 4)){
+                    if(minCost[currx][curry] < minCost[newx][newy]){
+                        dq.push_front({newx , newy});
+                        minCost[newx][newy] = minCost[currx][curry];
+                    }
+                }
+                else{
+                    if(minCost[currx][curry]+1 < minCost[newx][newy]){
+                        dq.push_back({newx , newy});
+                        minCost[newx][newy] = minCost[currx][curry]+1;
+                    }
                 }
             }
         }
