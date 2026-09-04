b284d05cca5a41171e3eb7a935e59cd120f62c31
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-04 11:44:40 +0530

    Time: 0 ms (100%), Space: 13.8 MB (52.09%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int r[4] = {0 , 1 , 0 , -1};
+    int c[4] = {1 , 0 , -1 , 0};
+    void doit(int currx , int curry , vector<vector<int>>& grid){
+        grid[currx][curry] = 2;
+        for(int k = 0 ; k<4 ; k++){
+            int newx = currx + r[k];
+            int newy = curry + c[k];
+            if(newx >=0 && newx < grid.size() && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == 0){
+                doit(newx , newy , grid);
+            }
+        }
+    }
+    void mark(int currx , int curry , vector<vector<int>>& grid , vector<vector<int>>& visited){
+        visited[currx][curry] = 1;
+        for(int k = 0 ; k<4 ; k++){
+            int newx = currx + r[k];
+            int newy = curry + c[k];
+            if(newx >=0 && newx < grid.size() && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == 0 && visited[newx][newy] == 0){
+                mark(newx , newy , grid , visited);
+            }
+        }
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if((i == 0 || j == 0 || i == m-1 || j == n-1) && (grid[i][j] == 0)){
+                    doit(i , j , grid);
+                }
+            }
+        }
+        int count = 0;
+        vector<vector<int>> visited( m , vector<int>(n , 0));
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(grid[i][j] == 0 && visited[i][j] == 0){
+                    mark(i , j , grid , visited);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
