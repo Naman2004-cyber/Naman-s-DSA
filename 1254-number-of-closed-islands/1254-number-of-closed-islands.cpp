93f327b228b5b4a0bb36eba840c280eeb0bf22cb
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-04 11:50:20 +0530

    Time: 2 ms (47.24%), Space: 13.1 MB (84.06%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-    int r[4] = {0 , 1 , 0 , -1};
-    int c[4] = {1 , 0 , -1 , 0};
+    int r[4] = {0 , 1 , 0 , -1} , c[4] = {1 , 0 , -1 , 0};
     void doit(int currx , int curry , vector<vector<int>>& grid){
         grid[currx][curry] = 2;
         for(int k = 0 ; k<4 ; k++){
-            int newx = currx + r[k];
-            int newy = curry + c[k];
-            if(newx >=0 && newx < grid.size() && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == 0){
-                doit(newx , newy , grid);
-            }
-        }
-    }
-    void mark(int currx , int curry , vector<vector<int>>& grid){
-        grid[currx][curry] = -1;
-        for(int k = 0 ; k<4 ; k++){
-            int newx = currx + r[k];
-            int newy = curry + c[k];
-            if(newx >=0 && newx < grid.size() && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == 0){
-                mark(newx , newy , grid);
-            }
+            int newx = currx + r[k] , newy = curry + c[k];
+            if(newx >=0 && newx < grid.size() && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == 0) doit(newx , newy , grid);
         }
     }
     int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m = grid.size() , n = grid[0].size();
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
-                if((i == 0 || j == 0 || i == m-1 || j == n-1) && (grid[i][j] == 0)){
-                    doit(i , j , grid);
-                }
+                if((i == 0 || j == 0 || i == m-1 || j == n-1) && (grid[i][j] == 0)) doit(i , j , grid);
             }
         }
         int count = 0;
         for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j<n ; j++){
                 if(grid[i][j] == 0){
-                    mark(i , j , grid);
+                    doit(i , j , grid);
                     count++;
                 }
             }
