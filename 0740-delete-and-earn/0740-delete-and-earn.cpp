ef92b8e629e7d852b09af613b268ef2aae02bb58
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-05 12:31:36 +0530

    Time: 0 ms (100%), Space: 15.8 MB (66.46%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int doit(int num , vector<int>& freq , vector<int>&dp){
-        if(num >= freq.size()) return 0;
-        if(dp[num] != -1) return dp[num];
-        int skip = doit(num+1 , freq , dp);
-        int take = 0;
-        if(freq[num] > 0) take = num * freq[num] + doit(num+2 , freq , dp);
-        return dp[num] = max(take , skip);
-    }
+    // int doit(int num , vector<int>& freq , vector<int>&dp){
+    //     if(num >= freq.size()) return 0;
+    //     if(dp[num] != -1) return dp[num];
+    //     int skip = doit(num+1 , freq , dp);
+    //     int take = 0;
+    //     if(freq[num] > 0) take = num * freq[num] + doit(num+2 , freq , dp);
+    //     return dp[num] = max(take , skip);
+    // }
     int deleteAndEarn(vector<int>& nums) {
         int maxi = *max_element(nums.begin() , nums.end());
         vector<int> freq(maxi+1 , 0);
-        vector<int> dp(maxi+1 , -1);
+        vector<int> dp(maxi+2 , -1);
         for(int i = 0 ; i<nums.size() ; i++) freq[nums[i]]++;
-        return doit(1 , freq , dp);
+        dp[maxi+1] = 0;
+        dp[maxi] = maxi*freq[maxi];
+        for(int i = maxi-1 ; i>=1 ; i--){
+            int skip = dp[i+1];
+            int take = 0;
+            if(freq[i] > 0) take = i*freq[i] + dp[i+2];
+            dp[i] = max(skip , take);
+        }
+        return dp[1];
     }
 };
