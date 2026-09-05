988de12d4cba67795bf59708f7f77352a4e85b2a
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-05 12:22:38 +0530

    Time: 0 ms (100%), Space: 16.4 MB (47.02%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int dp[10001];
-    int doit(int num , vector<int>& freq){
+    int doit(int num , vector<int>& freq , vector<int>&dp){
         if(num >= freq.size()) return 0;
         if(dp[num] != -1) return dp[num];
-        int skip = doit(num+1 , freq);
+        int skip = doit(num+1 , freq , dp);
         int take = 0;
-        if(freq[num] > 0){
-            take = num * freq[num] + doit(num+2 , freq);
-        }
+        if(freq[num] > 0) take = num * freq[num] + doit(num+2 , freq , dp);
         return dp[num] = max(take , skip);
     }
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> freq(10001 , 0);
-        memset(dp , -1 , sizeof(dp));
+        int maxi = *max_element(nums.begin() , nums.end());
+        vector<int> freq(maxi+1 , 0);
+        vector<int> dp(maxi+1 , -1);
         for(int i = 0 ; i<nums.size() ; i++) freq[nums[i]]++;
-        return doit(1 , freq);
+        return doit(1 , freq , dp);
     }
 };
