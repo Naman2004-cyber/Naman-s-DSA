2c98156854cdeeabcc0fc4f24b269197660e0a7b
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-05 15:28:52 +0530

    Time: 267 ms (36.02%), Space: 199 MB (33.13%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
     int r[4] = {0 , 1 , 0 , -1} , c[4] = {1 , 0 , -1 , 0};
-    bool doit(int day , int row , int col , vector<vector<int>> &water){
-        // put all the top row ones in the queue which are not filled with water
+    bool doit(int day , vector<vector<int>> &matrix , int row , int col){
         queue<pair<int,int>> qt;
-        int m = row , n = col;
         vector<vector<int>> visited(row , vector<int>(col , 0));
-        for(int j = 0 ; j<col ; j++){
-            if(water[0][j] > day) {
+        for(int j = 0 ; j<col ; j++) {
+            if(matrix[0][j] > day) {
                 qt.push({0 , j});
                 visited[0][j] = 1;
             }
         }
         while(!qt.empty()){
-            auto it = qt.front();
-            qt.pop();   
-            int currx = it.first , curry = it.second;
+            auto [currx , curry] = qt.front();
+            qt.pop();
             if(currx == row-1) return true;
             for(int i = 0 ; i<4 ; i++){
                 int newx = currx + r[i] , newy = curry + c[i];
-                if(newx < 0 || newy < 0 || newx >= m || newy >=n) continue;
-                if(water[newx][newy] <= day) continue;
-                if(visited[newx][newy] == 1) continue;
-                visited[newx][newy] = 1;
+                if(newx <0 || newy<0 || newx >=row || newy >=col || matrix[newx][newy] <= day || visited[newx][newy] == 1) continue;
                 qt.push({newx , newy});
+                visited[newx][newy] = 1;
             }
         }
         return false;
     }
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        vector<vector<int>> water(row , vector<int>(col , INT_MAX));
-        for(int i = 0 ; i < cells.size() ; i++){
-            water[cells[i][0]-1][cells[i][1]-1] = i+1;
-        }
-        int low = 1;
-        int high = cells.size();
-        int ans = 0;
+        vector<vector<int>> matrix(row , vector<int>(col , 0));
+        for(int i = 0 ; i<cells.size() ; i++) matrix[cells[i][0]-1][cells[i][1]-1] = i+1;
+        int low = 1 , high = cells.size() , ans = -1;
         while(low <= high){
             int mid = low + ((high-low)/2);
-            if(doit(mid , row , col , water)){
+            if(doit(mid , matrix , row , col)){
                 ans = mid;
                 low = mid+1;
             }
-            else{
-                high = mid-1;
-            }
+            else high = mid-1;
         }
         return ans;
     }
