304f200b9bf902b52f4dc4f2156b5b8204ae84ac
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-05 19:25:57 +0530

    Time: 10 ms (57.9%), Space: 235 MB (85.93%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.cpp
+++ 3840-house-robber-v/3840-house-robber-v.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     long long rob(vector<int>& nums, vector<int>& colors) {
-        vector<long long> dp(nums.size()+1 , -1);
-        dp[nums.size()] = 0;
-        dp[nums.size()-1] = nums[nums.size()-1];
+        long long nn = 0 ,  n = nums[nums.size()-1];
         for(int i = nums.size()-2 ; i>=0 ; i--){
-            long long skip = dp[i+1];
-            long long take = nums[i];
-            if(colors[i] != colors[i+1]) take+=dp[i+1];
-            else take+=dp[i+2];
-            dp[i] = max(take , skip);
+            long long skip = n , take = nums[i];
+            if(colors[i] != colors[i+1]) take+=n;
+            else take+=nn;
+            long long curr = max(take , skip);
+            nn = n;
+            n = curr;
         }
-        return dp[0];
+        return n;
     }
 };
