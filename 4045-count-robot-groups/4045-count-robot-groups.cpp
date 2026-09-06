412a60120e7b5e9cf4791a6d87739e63598aab78
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-06 12:36:49 +0530

    Time: 801 ms (5%), Space: 215.4 MB (28.97%) - LeetHub

---
+++ 4045-count-robot-groups/4045-count-robot-groups.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countGroups(vector<int>& position, vector<int>& speed, int distance) {
+        vector<int> pos;
+        vector<int> spee;
+        pos.push_back(position[0]);
+        spee.push_back(speed[0]);
+        for(int i = 1 ; i<position.size() ; i++){
+            if(position[i]-pos.back() <= distance) {
+                pos.back() = position[i];
+                spee.back() = speed[i];
+            }
+            else{
+                pos.push_back(position[i]);
+                spee.push_back(speed[i]);
+            }
+        }
+        for(int i = 0 ; i<pos.size() ; i++) cout << pos[i] << " ";
+        stack<int> st;
+        for(int i = 0 ; i<spee.size() ; i++){
+            while(!st.empty() && (spee[st.top()] > spee[i])){
+                st.pop();
+            }
+            st.push(i);
+        }
+        return st.size();
+    }
+};
