118afa497181159494511092c9f9f33f95a9cb74
Naman2004-cyber <namaniit2004@gmail.com>
2026-09-06 12:57:17 +0530

    Time: 0 ms (100%), Space: 202.7 MB (92.97%) - LeetHub

--- 4045-count-robot-groups/4045-count-robot-groups.cpp
+++ 4045-count-robot-groups/4045-count-robot-groups.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int countGroups(vector<int>& position, vector<int>& speed, int distance) {
-        vector<int> pos;
-        vector<int> spee;
-        pos.push_back(position[0]);
-        spee.push_back(speed[0]);
-        for(int i = 1 ; i<position.size() ; i++){
-            if(position[i]-pos.back() <= distance) {
-                pos.back() = position[i];
-                spee.back() = speed[i];
-            }
-            else{
-                pos.push_back(position[i]);
-                spee.push_back(speed[i]);
-            }
-        }
-        int mini = spee[spee.size()-1];
+        int mini = speed[speed.size()-1];
+        int minPosition = position[position.size()-1];
         int count = 1;
-        for(int i = spee.size()-2 ; i>=0 ; i--){
-            if(spee[i] <= mini){
+        for(int i = speed.size()-2 ; i>=0 ; i--){
+            if((speed[i] <= mini) && (minPosition-position[i] > distance)){
                 count++;
-                mini = spee[i];
+                mini = speed[i];
             }
+            minPosition = position[i];
         }
         return count;
     }
