8e34f89d34ec54de192de0c6f3b26bf74d470e75
NamanSharma72 <namaniit2004@gmail.com>
2026-09-09 16:56:55 +0530

    Time: 2 ms (85.69%), Space: 8 MB (76.08%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int mod = 1e9+7;
     int countHousePlacements(int n) {
-        vector<int> dp(n+1 , -1);
-        dp[n] = 0;
-        dp[n-1] = 1;
+        int NN = 0 , N = 1;
         for(int i = n-2 ; i>=0 ; i--){
-            int skip = dp[i+1];
-            int take = 1 + dp[i+2];
-            dp[i] = (take + skip)%mod;
+            int skip = N;
+            int take = 1 + NN;
+            int curr = (take + skip)%mod;
+            NN = N;
+            N = curr;
         }
-        int ans = 1 + dp[0];
+        int ans = 1 + N;
         int finalAns = (1LL * ans * ans)%mod;
         return finalAns;
     }
