db3e4f03e9f8965faad785cdd6d19b5eeadf2bb0
NamanSharma72 <namaniit2004@gmail.com>
2026-09-10 14:55:43 +0530

    Time: 67 ms (60.04%), Space: 17.1 MB (17.54%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int dp[100][201];
     int mod = 1e9 + 7;
-    int doit(int curr , int end , int fuel , unordered_map<int , vector<int>> &adj , vector<int>& locations){
-        // if(curr == end  && fuel == 0) return 1;
+    int doit(int curr , int end , int fuel , vector<vector<int>> &adj , vector<int>& locations){
         if(dp[curr][fuel] != -1) return dp[curr][fuel];
         int count = 0;
         if(curr == end) count = 1;
@@ -15,7 +14,7 @@ public:
         return dp[curr][fuel] = count;
     }
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        unordered_map<int , vector<int>> adj;
+        vector<vector<int>> adj(locations.size());
         for(int i = 0 ; i<locations.size() ; i++){
             for(int j = 0 ; j<locations.size() ; j++){
                 if(j != i && abs(locations[j]-locations[i]) <= fuel) adj[i].push_back(j);
