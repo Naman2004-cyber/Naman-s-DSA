b06714c0f9a79de08a42b36500d11d807ad20aa3
NamanSharma72 <namaniit2004@gmail.com>
2026-09-11 14:12:55 +0530

    Time: 19 ms (14.61%), Space: 20.7 MB (9.75%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    unordered_set<string>contains;
-    int dp[301];
-    bool doit( string&s , int idx)
-    {
-        if( idx == s.size()) return 1;
-        string formed="";
-        if(dp[idx] != -1) return dp[idx];
-        for( int i=idx ; i<s.size(); i++)
-        {
-            formed+=s[i];
-            if(contains.find(formed) != contains.end())
-            {
-                if(doit(s,i+1)) return dp[idx] = 1;
-            }
+    int dp[300][300];
+    int doit(int idx , int start , unordered_map<string , int> &mpp , string &s){
+        if(idx == s.size()){
+            string strNew = s.substr(start , idx-start+1);
+            if(mpp.find(strNew) != mpp.end()) return 1;
+            return 0;
         }
-        return dp[idx] = 0;
+        if(dp[idx][start] != -1) return dp[idx][start];
+        string strNew = s.substr(start , idx-start+1);
+        int conti = doit(idx + 1 , start , mpp , s);
+        int take = 0;
+        if(mpp.find(strNew) != mpp.end()){
+            take = doit(idx+1 , idx+1 , mpp , s);
+        }
+        return dp[idx][start] = conti || take;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        memset(dp,-1,sizeof(dp));
-        for( string &word : wordDict) contains.insert(word);
-        return doit(s,0);
-        
+        unordered_map<string , int> mpp;
+        for(int i = 0 ; i<wordDict.size() ; i++) mpp[wordDict[i]]++;
+        string str = "";
+        memset(dp , -1 , sizeof(dp));
+        return doit(0 , 0 , mpp , s);
     }
 };
